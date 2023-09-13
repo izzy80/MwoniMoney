57a7462b94a7c543474eaa0644684f49acb486a8
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-13 11:03:27 +0900

    feat : BaseEntity 설계
    Former-commit-id: 7137edf529e72abf2f9b8bc23d1e9d624f2f3d0c

---
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/challenge/Challenge.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:021cc5c3bc57a89bb904fcf7f19b6f8d29cf84d94bcb8410f661771036706d44
+size 83
---
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/challenge/MemberChallenge.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5759185f688445de22bff3d980c5021bbb41eef01afae1b6cd000e1c08cff0f1
+size 89

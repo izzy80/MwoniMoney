157a061345ffcd0244a70dfb5100ef21ef6013ea
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-13 16:52:50 +0900

    [S09P22B310-103] feat : challenge entity 설계
    Former-commit-id: 0a1f910f65f8e2bc3a244fff7a3a6128d1d55694

--- wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/BaseEntity.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5735e2b209f6b421759881666b921c152f36aa1989cd76372df001616841d8e3
-size 311
--- wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/challenge/Challenge.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:846a094ac8a48daa49e8e26a68aca147e6e7b3a29956387c8102ff39c181aaaf
-size 415

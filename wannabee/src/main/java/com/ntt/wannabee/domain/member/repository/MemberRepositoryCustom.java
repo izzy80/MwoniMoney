8beae3434294e0a97a7031915b7388d1963d4a04
tjdfkr011 <tjdfkr011@naver.com>
2023-09-13 17:34:45 +0900

    [S09P22B310-71]feat : Entity 구조 변경
    Former-commit-id: a3c5c4eff6e31429ab702f141028ade2015db738

--- wannabee/src/main/java/com/ntt/wannabee/domain/member/repository/MemberRepositoryCustom.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/repository/MemberRepositoryCustom.java
@@ -1,9 +1,3 @@
-package com.ntt.wannabee.domain.member.repository;
-
-import java.util.Optional;
-
-import com.ntt.wannabee.domain.member.entity.Member;
-
-public interface MemberRepositoryCustom {
-	Optional<Member> findMemberByUuid(String uuid);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4f37cc8fb242d72af13aa6826cf7104a6a19e81d8c995283e7d3f92cbbbea40c
+size 227

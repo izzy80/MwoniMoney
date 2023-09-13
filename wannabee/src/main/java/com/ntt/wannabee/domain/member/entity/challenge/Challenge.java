e3496a45eb1ebc132b154c3a7adb7ee9a83687b0
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-13 11:06:02 +0900

    feat: Challenge 설계
    Former-commit-id: a76006a0c914e1eb6a0dc28dae87c622491d9db4

--- wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/challenge/Challenge.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/challenge/Challenge.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:021cc5c3bc57a89bb904fcf7f19b6f8d29cf84d94bcb8410f661771036706d44
-size 83
+oid sha256:846a094ac8a48daa49e8e26a68aca147e6e7b3a29956387c8102ff39c181aaaf
+size 415

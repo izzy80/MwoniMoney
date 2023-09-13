5b07f38cb064e5045a9e05c26a12c9a129c1d662
tjdfkr011 <tjdfkr011@naver.com>
2023-09-13 15:37:18 +0900

    feat : CRLF -> LF로 변경
    Former-commit-id: 4841d618e422bc028799e0db8ac1d0c1f2e5a9aa

--- wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/Child.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/Child.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3cc6d5b0931c2ca9ab22953984d1d90d883d00c6fb70b5687684c53bccee6c0
-size 999
+oid sha256:b6ff67f1374562f023fa1ccfd33b8cdb52caecf6d21b46bb356f2fb4cf48d89d
+size 1000

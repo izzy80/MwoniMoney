e766f0dc6419641bbbea7fce0e40049ef28b6f1b
tjdfkr011 <tjdfkr011@naver.com>
2023-09-13 16:01:43 +0900

    feat : CreateTime 컬럼 추가
    Former-commit-id: d8258fdde3d64532db90eba6ce6e94dbd2c808df

--- wannabee/src/main/java/com/ntt/wannabee/domain/member/repository/MemberRepositoryCustom.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/repository/MemberRepositoryCustom.java
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4f37cc8fb242d72af13aa6826cf7104a6a19e81d8c995283e7d3f92cbbbea40c
-size 227
+package com.ntt.wannabee.domain.member.repository;
+
+import java.util.Optional;
+
+import com.ntt.wannabee.domain.member.entity.Member;
+
+public interface MemberRepositoryCustom {
+	Optional<Member> findMemberByUuid(String uuid);
+}
--- wannabee/src/main/java/com/ntt/wannabee/domain/member/service/MemberService.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/service/MemberService.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:da51232944fa31243922e77319417cba04a4877a0608f8e61e7e27ec9ccf80fe
-size 343
+package com.ntt.wannabee.domain.member.service;
+
+import com.ntt.wannabee.domain.member.model.vo.SmallAccount;
+
+public interface MemberService {
+
+	public void changeNickname(String newNickName);
+
+	public void addSmallAccount(SmallAccount smallAccount);
+
+	public void finishSmallAccount(String uuid);
+
+	public void getMemberInfo(String uuid);
+}

d589514280c78624f8ab0395a18f964132db6c1b
tjdfkr011 <tjdfkr011@naver.com>
2023-09-13 17:37:26 +0900

    [S09P22B310-71]feat : Entity 구조 변경
    Former-commit-id: 282633c22f01b21c6f5003a6f2b57087253adc07

--- wannabee/src/main/java/com/ntt/wannabee/domain/game/model/dto/BalanceGameHistoryDto.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/game/model/dto/BalanceGameHistoryDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b19a2b39277b8efd24f8fe99e47024e4e1f16b82d1c1747c25d847fac540269
-size 236
+oid sha256:8b53a54f8f4b5a2bdb257ad5aa788289b04593353778cf484f0578cf7b12389b
+size 233

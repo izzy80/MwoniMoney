8d6d7a56814a731eb310e776aa3eb423eb60fe3e
tjdfkr011 <tjdfkr011@naver.com>
2023-09-13 16:02:32 +0900

    feat : CreateTime 컬럼 추가
    Former-commit-id: 97c95cc631bc780caa77534e162f41f8eb6f21f5

--- wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/Member.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09c3136ab1a8d74eb0fa1ded78efb0862e4b96345894f3a6fd23eebfe9c9abc0
-size 2271
+oid sha256:cc88054f11f5f0e8feda05819f5e371bb48ebfac015754a42519b6de6a501e67
+size 2350

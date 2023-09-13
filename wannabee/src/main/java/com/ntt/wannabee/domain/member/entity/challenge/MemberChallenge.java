4a85848d202229c1f5e25659a25ec0fd72ae0d1e
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-13 11:06:29 +0900

    feat:MemberChallenge 설계
    Former-commit-id: 6049ed9578a2a129c9328e4f5dfd839fd4ed737a

--- wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/challenge/MemberChallenge.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/member/entity/challenge/MemberChallenge.java
@@ -1,2 +1,3 @@
-package com.ntt.wannabee.domain.member.entity.challenge;public class MemberChallenge {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3cf256b3b9406eb2af9428d641715b982d353865b7725b2686fceb146c44ee7
+size 813

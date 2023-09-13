4c401f2e63107824fd0de27d32361b353e7db3fb
tjdfkr011 <tjdfkr011@naver.com>
2023-09-13 16:17:17 +0900

    feat : Builder 수정
    Former-commit-id: 30041fe84002d74714b1e4073a542c8e215e4d0e

--- wannabee/src/main/java/com/ntt/wannabee/domain/game/entity/BalanceGameHistory.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/game/entity/BalanceGameHistory.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e52ad96f9047615aa11d92276db61a2ec0cf2e306c79d1a060f0a8f238882d8
-size 1828
+oid sha256:0b19c661362473a157175caca9d67248ddabf17c2feec0559fcd1795e48067bd
+size 1706

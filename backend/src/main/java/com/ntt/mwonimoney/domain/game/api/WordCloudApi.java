b978bde1958ed97077eb6a6119ca9e17528375fc
tjdfkr011 <tjdfkr011@naver.com>
2023-09-14 17:06:42 +0900

    [S09P22B310-083] feat : 멤버 로그인 서비스 프로토타입 구현
    Former-commit-id: c2d5ccbf64fe322ec96ed5af80d4e41c96d0db04

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/api/WordCloudApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/api/WordCloudApi.java
@@ -1,4 +1,3 @@
-package com.ntt.mwonimoney.domain.game.api;
-
-public class WordCloudApi {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8709b1b1e0ed6e38a7246b5f47a241ee421b05bf1f2e110f09205bb04b9a89c3
+size 75
---
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/model/dto/MemberAuthDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6671d14d6909d098d17245cd0cd94fb44ae3321f3a1a60f6124a9d350c9a8ff1
+size 232

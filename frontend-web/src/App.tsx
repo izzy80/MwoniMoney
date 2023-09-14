1875936f59e431d1155f19671b073451cd0e6991
박기택 <taekputer@gmail.com>
2023-09-14 16:27:17 +0900

    [S09P22B310-000] config : react 설정
    Former-commit-id: b5e74b71408f2e2b67e41f595539a56e1f6de76b

---
+++ frontend-web/src/App.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c5804ec8ea015b0b25fed0ce39101ad3ad357ba1d92efb02297efe0e497445c
+size 122
---
+++ frontend-web/src/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:062971dd99b5b628e53b5fcfe811e607d196b8c655b41ae8566448465200ca74
+size 286

d05d46607a13a46d180cdcbb573873c42e3585b4
조성락 <tjdfkr011@naver.com>
2023-09-14 17:47:23 +0900

    Revert "[S09P22B310-071] feat : @Service 어노테이션 인터페이스만 붙임"
    This reverts commit 926766acaa24e42b87c92358e3220fba3b8ab988 [formerly 257d0aecdb58cbfe91ee3447e01a9d297d8f9dd0]
    
    Former-commit-id: 8090c2a67fee67e5e9a6b0067419d6a4ede83304

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameHistoryServiceImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameHistoryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72485ec0294e71ededdc53e16e1a90927b8bfada0c91801bae22d2ebbee0704e
-size 1297
+oid sha256:580d521158cf0f32fdc5d3a726f9dc6113b134ce9a168921437c011f73ebb9d1
+size 1353

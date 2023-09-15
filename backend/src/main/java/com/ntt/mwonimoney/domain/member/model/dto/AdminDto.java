bc0522b46801d04735724d653b5a6ed1e3a4a464
윤민재 <w124564@naver.com>
2023-09-15 16:49:31 +0900

    [S09P22B310-000] remove : admin 삭제
    Former-commit-id: a49bfcdb670c686ed037d85d995715e93d73eb7a

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/model/dto/AdminDto.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:942e1dc9d22415d27fdf3b97df603fb596cb9e2bae89911eda4875524ed65e4d
-size 251
--- backend/src/main/java/com/ntt/mwonimoney/domain/member/model/vo/MemberRole.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/model/vo/MemberRole.java
@@ -1,5 +1,3 @@
-package com.ntt.mwonimoney.domain.member.model.vo;
-
-public enum MemberRole {
-	PARENT, CHILD, ADMIN
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4bfc8128cacffab17e6cfb46943f232ec8bf15e354dc1cf4ca7b18c973503986
+size 94

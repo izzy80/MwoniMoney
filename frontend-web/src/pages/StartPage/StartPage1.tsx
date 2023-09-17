df95825dbd86fcb57e154f57d5753ed6b1f89b76
Jaei <dobbykim0320@gmail.com>
2023-09-17 18:26:13 +0900

    [S0P22B310-135] Rename : 이미지 설정파일 생성d
    Former-commit-id: 088682004d758ea61f685a48c651d403f2c2ef3e

--- frontend-web/src/pages/StartPage/StartPage1.tsx
+++ frontend-web/src/pages/StartPage/StartPage1.tsx
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5dde202f55cdf855db0d4e6669efb6a0ff96d7b9c21149110be4a8f0cb8db194
-size 537
+import React from "react";
+
+function MyComponent() {
+  return <div>ddd</div>;
+}
+
+export default MyComponent;

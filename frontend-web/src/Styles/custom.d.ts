5ad9b959507197dd5a76e12ac0bffbbdf3c9c507
Jaei <dobbykim0320@gmail.com>
2023-09-17 18:26:13 +0900

    [S0P22B310-135] Rename : 이미지 설정파일 생성d
    Former-commit-id: 291168ce701b9874315a99d8383bb8e8e5f0d2f5

---
+++ frontend-web/src/Styles/custom.d.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7d4fccb97aaa1c5bdbf103f319bcb144b5bea684f009f06d6c2376438155dc8
+size 384
--- frontend-web/src/pages/StartPage/StartPage1.tsx
+++ frontend-web/src/pages/StartPage/StartPage1.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dde202f55cdf855db0d4e6669efb6a0ff96d7b9c21149110be4a8f0cb8db194
-size 537
+oid sha256:8bef447fbae7aa61adcf93a7e3ecf86108c5e3cab118befc223e0fb00201ef3f
+size 109

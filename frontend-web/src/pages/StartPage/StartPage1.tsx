e338555758bdc43cc97815352dd12e827c0a7beb
taekto <taekputer@gmail.com>
2023-09-17 19:26:30 +0900

    [S09P22B310-114] Design : 디자인 변경
    Former-commit-id: 35c4ab449e2c1ef92062208e2d282c83c783ccba

--- frontend-web/src/pages/StartPage/StartPage1.tsx
+++ frontend-web/src/pages/StartPage/StartPage1.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-
-function MyComponent() {
-  return <div>ddd</div>;
-}
-
-export default MyComponent;
+version https://git-lfs.github.com/spec/v1
+oid sha256:26168deea8535a47c78d0cd2539ca69fd7ce3540393d80e5dc2d33f5ec9f4fc2
+size 1390

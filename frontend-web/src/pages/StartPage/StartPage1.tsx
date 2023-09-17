7f85832e95f38ef947866b5c0717728dbeeae57a
taekto <taekputer@gmail.com>
2023-09-17 21:22:18 +0900

    [S09P22B310-114] Design : 디자인 변경
    Former-commit-id: b2ec3f718a7b84c7432e263e2b42ebc644bc9fdc

--- frontend-web/src/pages/StartPage/StartPage1.tsx
+++ frontend-web/src/pages/StartPage/StartPage1.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26168deea8535a47c78d0cd2539ca69fd7ce3540393d80e5dc2d33f5ec9f4fc2
-size 1390
+oid sha256:544167f192a2c375b34e3f0bd7499a87a0ffba98b55797541cf0885b9a01b7e5
+size 1994

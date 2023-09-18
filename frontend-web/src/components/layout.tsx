f1afc06c195beca8778351234e5e4858d69c6d70
박기택 <taekputer@gmail.com>
2023-09-18 15:22:34 +0900

    [S09P22B310-000] config : react 설정
    Former-commit-id: efaa6802a97b5677aa1908b128b8999a94a94984

--- frontend-web/src/components/layout.tsx
+++ frontend-web/src/components/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6abb6fb0d02b7353812facb35490b94f02c28999879a84dabb496564220dd92f
-size 243
+oid sha256:a95bc9bcd3d95a69c441bbbaf5f33c60be173f6fe606da235b19f10c31ebdfdc
+size 270
--- frontend-web/src/pages/Common/MoneyPage.tsx
+++ frontend-web/src/pages/Common/MoneyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7541e55d4e51fccd52c54a88d8c0b802df55b79a8545c6e7907ab2751dbd907
-size 707
+oid sha256:f4deb91da82bbcde4bc735003426a1edd46260aaa6f75bfc8f6dd44965006dc3
+size 783

e50807f4f17818fdb42d8e57dd6e381091ee28af
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-18 09:22:06 +0900

    [S09P22B310-154] feat : rChallenge @Column 추가
    Former-commit-id: 26cdc512923db0278f9c78e055b8d8a8ac1e553a

--- backend/src/main/java/com/ntt/mwonimoney/domain/challenge/entity/Challenge.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/challenge/entity/Challenge.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6eaac357ee5f1e02d65c16dfdb7d9ad61846cedb5ad29542866b733c884818c
-size 534
+oid sha256:c3f8e545922c4092b8e7d54a8eb6340d69fe10912ca9acf812397ff0f92be8e1
+size 754

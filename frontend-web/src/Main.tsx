f6950c2ce7cae25131cedbf44e79a72f05f2e025
박기택 <taekputer@gmail.com>
2023-09-18 13:11:46 +0900

    [S09P22B310-000] config : react 폴더 추가
    Former-commit-id: d766a4c7f7ca52e7f35e399660f5d1c7fcbc5517

--- frontend-web/src/Main.tsx
+++ frontend-web/src/Main.tsx
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:51788c3ae4edaf5876410ac58428990b8928309e46a3815e0a807fa49db022c7
-size 115
+import React from "react";
+
+function Main() {
+  return <div>사람입니다.</div>;
+}
+
+export default Main;

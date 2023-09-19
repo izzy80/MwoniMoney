334122317bec655365313b6d45113025d6cd87ae
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-19 23:30:40 +0900

    [S09P22B310-195] Design: 퀴즈 css
    Former-commit-id: 178dfd7a5f97a3612355fdd064e695c2cf6e0624

---
+++ frontend-web/src/components/Children/Challenge.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:798839346b02123286c730b317360181cbf2b6f7e50e1aa3e38f3892847a5042
+size 104
--- frontend-web/src/pages/ChildrenPage.tsx
+++ frontend-web/src/pages/ChildrenPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c6c05979f774dfa78ae4641a43007883d42e0cdcee276cf7264c7063e860afd
-size 135
+oid sha256:472848509d14b9b14999beebb8d140ed73b1f4d59bae32b2b5cba86b311b8e4f
+size 1508

606324db1711c5d5ef3165c92b590792b08e0757
박기택 <taekputer@gmail.com>
2023-09-19 15:13:40 +0900

    [S09P22B310-179] config : 파일 생성
    Former-commit-id: 6a3a2dedff1f0c5de75b5a46fc1db525c3b534f4

--- frontend-web/src/pages/Common/MoneyPage.tsx
+++ frontend-web/src/pages/Common/MoneyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d8d7db2aabcadf0fe4fb899c1b6d87984d85f2b959001c05a084cec20776ebb
-size 5451
+oid sha256:3941da8cf53bd7ec4913f9a6a005959b122f3bb073a1f8676fd07830cc4d24de
+size 5234

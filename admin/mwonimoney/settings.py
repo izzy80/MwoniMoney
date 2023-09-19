22749f5f31b66b1e46410d1d357cf90301d96be4
aldms8960 <45choi22@gmail.com>
2023-09-19 17:38:03 +0900

    Fix: nginx 설정으로 인한 ALLOWED_HOST 설정 수정
    Former-commit-id: 40620403f41cd5dbff7740e794a0b4a617d82d2f

--- admin/mwonimoney/settings.py
+++ admin/mwonimoney/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1df9b9ba68218775f6c11fc1a5dbc3307181c2c50f99fb9307a6a506ae5cc83
-size 3285
+oid sha256:d7ade5db728c867dbbc32bf3e859819566046aae77bef343198839453efa3733
+size 3288

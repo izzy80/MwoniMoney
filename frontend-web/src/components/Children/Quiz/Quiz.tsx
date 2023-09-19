e05061372b6ee2d4981f5f49d137902cf19498d0
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-19 22:33:54 +0900

    [S09P22B310-195] Refactor : 부모와 자식 페이지 변수로 렌더링될 수 있게 수정
    Former-commit-id: c027b7ac859737b3918626a628596e9eda54915e

--- frontend-web/src/components/Children/Quiz/Quiz.tsx
+++ frontend-web/src/components/Children/Quiz/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d78b8d0de49fd0b416bce91fcd0aeec2cf7c2821b09763cec45d2bab8f8ed32a
-size 1042
+oid sha256:cfb6b24385a6cf1fca4bd1f8e71d7914cdf5a2cab4a979e1c7e003159726aead
+size 1043

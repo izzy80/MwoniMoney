8ef842a46196b2d5fef6eaa0a5e2b57ac560fb1d
Jaei <dobbykim0320@gmail.com>
2023-09-20 14:17:40 +0900

    [S09P22B310-229] Design :공통 컴포넌트 작업
    Former-commit-id: 614297f31997d2844d2847cd25f39d6e608e164f

---
+++ frontend-web/src/components/Common/AboutText.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39d7674a92c8c8f110b71e7aec8aa20b6aed9ac637adc1ff81df81021371b46c
+size 670
---
+++ frontend-web/src/components/Parents/Bank/GoGoalMoney.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f7c0d5c7fa0a71d49238e3905877c3aa208fa89fb6e793e9d37bd6c978646ef5
+size 993

e571bc984b9b68833d8d8788752d88fc6c5ecbae
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-20 17:07:10 +0900

    [S09P22B310-190] design: 자식 페이지 css text props 수정
    Former-commit-id: 3e97abbe050264413f926b36b215453487972210

--- frontend-web/src/components/Children/Main.tsx
+++ frontend-web/src/components/Children/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6794e8ecb86a361a78a4619caa547c481a3db0508423fb57cc5134afa202ec58
-size 2784
+oid sha256:dac7bff881c90a86e8ebaa9151c50135c5e3f0806f68a958afe172582ad6633f
+size 2934

65d6d677533fb525c1ed4b6773e8709c2893ebc0
Jaei <dobbykim0320@gmail.com>
2023-09-20 14:17:40 +0900

    [S09P22B310-229] Design :공통 컴포넌트 작업
    Former-commit-id: ea950b0d415aafdc38fe90d05058b8971f9e39f0

--- frontend-web/src/components/Layout/parentsLayout.tsx
+++ frontend-web/src/components/Layout/parentsLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71688242e195a7efdee8d882b6c4cb2789de9a02e8025e22dfb7e823439b3ae3
-size 349
+oid sha256:f46dddbd58db5437f242025a5ddd470210f62d64c8f3d9ee90db4add26d6378d
+size 453

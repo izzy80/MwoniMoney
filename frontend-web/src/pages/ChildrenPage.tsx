63e6aaf7c5fb6b823d79baeb0bd7720f40fd16c5
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-20 09:01:48 +0900

    [S09P22B310-195] design: 퀴즈 css
    Former-commit-id: ce5ebc2c134a283b44c93831aba6d54906015250

--- frontend-web/src/pages/ChildrenPage.tsx
+++ frontend-web/src/pages/ChildrenPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:472848509d14b9b14999beebb8d140ed73b1f4d59bae32b2b5cba86b311b8e4f
+oid sha256:b4d01a1a44f4fe60053db89164d192e7eca004a7274c86e84f3c363112d96b10
 size 1508

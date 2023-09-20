43aeef0348d156311cce916f866180242a1c4c79
aldms8960 <45choi22@gmail.com>
2023-09-20 12:29:35 +0900

    Fix: csrf forbidden오류로 인한  설정 수정
    Former-commit-id: e25a8a3209050b5f65776a8b58ca274278f10a9e

--- admin/mwonimoney/settings.py
+++ admin/mwonimoney/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:135de07397a3cb9187f5341e5c0720bd99c4c95d624e848e7df71bf778be4a51
-size 3618
+oid sha256:5da2fdddbd165b0b973d7c65b81bcc1ac4e10ae081ac7686692e9b13c5a150c5
+size 3526

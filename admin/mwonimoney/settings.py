7955eea37e4cc0ddd41644b77133f6970e43648b
aldms8960 <45choi22@gmail.com>
2023-09-20 09:22:52 +0900

    Fix: static_files 설정 수정
    Former-commit-id: 9eaeb84b5b559ed5a7c8891b0ae20d7aafa8ba9f

--- admin/mwonimoney/settings.py
+++ admin/mwonimoney/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac157793c368a442918ee21baa7af5f79491a64b5be3e35c9f6a98fa326f33ab
-size 3437
+oid sha256:120635abfa4e03d7c75eb847ab86e0bd5b6e9e3949644b83147c4f34141d0954
+size 3443

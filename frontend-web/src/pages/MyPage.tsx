06580690bd8dd12ba5a8948cfb54816794089450
박기택 <taekputer@gmail.com>
2023-09-20 17:21:10 +0900

    [S09P22B310-178] feat : myPage css v2!
    Former-commit-id: 77f0cc31318fd55f7168c1cc4209854484fadf61

--- frontend-web/src/pages/MyPage.tsx
+++ frontend-web/src/pages/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a14e77229b78d400e9c4b496e3ab0ffa6f4187d0f4e3963831281b5e1da4fc11
-size 5079
+oid sha256:c5a0af703e088117f1cf37f9c6e413da18e229e702ab4027e7cbf08f1553a19a
+size 5692

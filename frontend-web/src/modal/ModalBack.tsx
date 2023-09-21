2f7506d01f266f9fa232c9424f9020f3bb586b21
Jaei <dobbykim0320@gmail.com>
2023-09-21 15:17:04 +0900

    [S09P22B310] Fix : 모달 사용 편의성 보안
    Former-commit-id: 81531c6f4e17a56a1d18dfdbcfbb294683aa2a2b

--- frontend-web/src/modal/ModalBack.tsx
+++ frontend-web/src/modal/ModalBack.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d6ae908b3617ee6324a266b466c57db5d27b05aae05a2e529f3c36306216f42
-size 2258
+oid sha256:332ba5588b0b22c7fa46c1c353605dc3b68f4d84e3e6368c43933dcb074e0c84
+size 2411

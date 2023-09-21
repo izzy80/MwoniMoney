511fe5cd88ac956901a01813c01d9085fbb0bfed
Jaei <dobbykim0320@gmail.com>
2023-09-21 15:17:04 +0900

    [S09P22B310] Fix : 모달 사용 편의성 보안
    Former-commit-id: f7427441dcf7eac5682027dacc99160f61caca1e

--- frontend-web/src/modal/Quiz/Success.tsx
+++ frontend-web/src/modal/Quiz/Success.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e531b6e361adb67a53ab52797fd306d3e48bb80f51664b25ba443b1d0ec763f3
-size 369
+oid sha256:0b061a6557c2bc2b8db9eb84c8494e0d4b13f0765901feca8cc499286d65897c
+size 478

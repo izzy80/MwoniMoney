90a04f224dcd4472291fdb971505b8e4179f41d4
Jaei <dobbykim0320@gmail.com>
2023-09-22 09:40:44 +0900

    [S09P22B310-232] Config : 폴더 정리
    Former-commit-id: 0590d57808eeb76c3ffbdd11d86380badafb61c5

--- frontend-web/src/pages/Balance.tsx
+++ frontend-web/src/pages/Balance.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c5c7c09d14ab31b6325941b9b3b21ac0933d6fcbf54398e2c5f99535265eef8
-size 2514
+oid sha256:4806c973b3529324220ec01229c3bc28cc367e2e9c0ce3920e766b3016f3a9a9
+size 2541

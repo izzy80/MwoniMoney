7d3504e38f06d2c5968ab4bfe5c0551f1b06bf59
Jaei <dobbykim0320@gmail.com>
2023-09-22 17:49:59 +0900

    [S09P22B310-233] Design : 용돈기입장 수정 중
    Former-commit-id: 88e7c225372aea44c9af985106bb7afcf131d6f0

---
+++ frontend-web/src/components/Common/MoneyPage/MoneyTable.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f57bcb8344b9042cadbbde165dbf1f3fa225075a2eb366653b075224ebb30496
+size 1090
--- frontend-web/src/components/Layout/layout.tsx
+++ frontend-web/src/components/Layout/layout.tsx
@@ -1,11 +1,3 @@
-import React from "react";
-import ChildrenLayout from "./childrenLayout";
-import ParentsLayout from "./parentsLayout";
-
-function Layout() {
-  //number = 1 : 부모 , number = 0 : 자식
-  const role: number = 0;
-  return <>{role === 1 ? <ParentsLayout /> : <ChildrenLayout />}</>;
-}
-
-export default Layout;
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b5643b2a10499d145d470c6e0cc46eb9eb4e394997555a0140345c1765e35d4
+size 307

8e8580b2544c700e9f72b3ed623ff12a7aa3e432
tjdfkr011 <tjdfkr011@naver.com>
2023-09-22 10:33:31 +0900

    [S09P22B310-000] feat : 레디스 config 파일 삭제
    Former-commit-id: a51c7289e10f4372e171650122b7e818fe2a7aeb

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/model/vo/BalanceGameAnswer.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/model/vo/BalanceGameAnswer.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c0a1563b4ad8d869144473f2a6f95740accccc597132c057f67432cb5ae846d2
-size 96
+package com.ntt.mwonimoney.domain.game.model.vo;
+
+public enum BalanceGameAnswer { LEFT, RIGHT
+}

80d7c254db56594b88044d019f2b83f9ab4790fd
tjdfkr011 <tjdfkr011@naver.com>
2023-09-22 21:44:07 +0900

    [S09P22B310-198] feat : 자녀 엔티티 구조 변경 및 컨트롤러 구현
    Former-commit-id: 93cf3d56653e6bbd314c45b4db2afdd63e43a063

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/api/ChildrenApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/api/ChildrenApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:680af85ed728984c9f603b47c1516a1d32acad3f08354eb6874df09d417683e9
-size 2089
+oid sha256:feaff98314733bc1b76956401501b8bd994266470cfcd6fab09b2529b1f4c218
+size 2158

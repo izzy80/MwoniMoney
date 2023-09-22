1e04fe586d7a91c4a3f789a03665b430536e7f8d
aldms8960 <45choi22@gmail.com>
2023-09-22 14:31:20 +0900

    Feat: Balance, Challenge, Quiz 모델 관리 - 생성 옵션 삭제
    Former-commit-id: 667a1f846356bfc104f882e04a6272c49d1911ff

--- admin/mwonimoney/models.py
+++ admin/mwonimoney/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:064385b98fb5e24cbea1736690dce2cb0a3ef15b9d587e42ca64dc7d23ed5508
-size 1601
+oid sha256:00a788364faa783106b6470c85e9e36f11042dc59a0bab779cf033b79f846ec0
+size 1595

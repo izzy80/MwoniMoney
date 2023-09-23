41006a2aca2389c1ab62de63cf1d2bfb42bfee50
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-23 17:25:24 +0900

    [S09P22B310-185]design:faq css
    Former-commit-id: b8b125287794095e8d2b1517ca572add8761e2f1

--- frontend-web/src/pages/Faq.tsx
+++ frontend-web/src/pages/Faq.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a1106a937c53062c82e81d7945f0b7bfa9f9aeb489563fbfe7628d4f2243f69
-size 1255
+oid sha256:bcbec5565ae2b29f92f497177f5830f46a3ea7799a5ad7e78948ea6b8c5ca73f
+size 2776

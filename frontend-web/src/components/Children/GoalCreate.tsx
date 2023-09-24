b3555334949409a86c1750738223eaa1b663eb6d
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-24 19:35:46 +0900

    [S09P22B310-179] design: 짜금통 생성 페이지
    Former-commit-id: b32989e552c10e895a7dd28a18edaa6bfa5420f3

--- frontend-web/src/components/Children/GoalCreate.tsx
+++ frontend-web/src/components/Children/GoalCreate.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8eb9a2296de30e217b819b2f583f5a14d320073543913a9d853e69f88cd6783f
-size 2678
+oid sha256:f061ddc3a893aedf06cd46f275d5fefad587ef77294f8e546da9ba3d06d09c2a
+size 4408

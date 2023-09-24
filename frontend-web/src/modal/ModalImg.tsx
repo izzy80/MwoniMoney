8a4148479e0a94e73576858e11c2903a38da0584
Jaei <dobbykim0320@gmail.com>
2023-09-24 15:56:18 +0900

    [S09P22B310-135] 짜금통 푸터에서 빼고 main에 삽입
    Former-commit-id: d45563ba1e55792bbe3b121b0b4a3d80d21721a3

--- frontend-web/src/modal/ModalImg.tsx
+++ frontend-web/src/modal/ModalImg.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ec71eb1a2e733465e24258ff4e9e1a5730e191ed149a962068ab29e23aad09e
-size 3311
+oid sha256:2103c6c16b159286d99621ceaf1aa9049b9ee84e02437c672aabec0ec4df2ee6
+size 3125

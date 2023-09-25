f08b128e44087c9b931c78a77deeb10a3e30872b
Jaei <dobbykim0320@gmail.com>
2023-09-25 13:27:24 +0900

    [S09P22B310-263] Feat : 모달 handleOpen
    Former-commit-id: b33851ed2bcc52478277385077e8ff37324011a0

--- frontend-web/src/components/Parents/Main.tsx
+++ frontend-web/src/components/Parents/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce9cbf9670d2c76d6bbd29a9fe55ed149074843c7e3eb2a35c373d59d4b8a535
-size 1779
+oid sha256:9b2b3270c26252f352c9b2b2235ef3e278b9462aab6fd90240488f219656a466
+size 1763

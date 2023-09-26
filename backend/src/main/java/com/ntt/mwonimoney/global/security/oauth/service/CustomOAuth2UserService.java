d2dfe5f29887fae278fd178ac14ffe2d48bd1c3e
윤민재 <w124564@naver.com>
2023-09-26 12:54:58 +0900

    [S09P22B310-000] fix : 로그 출력
    Former-commit-id: d9760a46f2d90662102a7658264672533b2951fe

--- backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/service/CustomOAuth2UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8a57760eee1822a97ca3b6cea175512e728fd43b66303dd0ffcbd8247d1b82f
-size 2644
+oid sha256:29d2eb1c86f3192efe6b6b7403da2296fd48bf708b71b2afa98e7716c003aaec
+size 2693

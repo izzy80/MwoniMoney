146cbfbdf2d72965e2c66eb61d1ac0e7a28b7ec7
윤민재 <w124564@naver.com>
2023-09-26 12:54:58 +0900

    [S09P22B310-000] fix : 로그 출력
    Former-commit-id: ce7813f8a27860520e233ee1705f9bfc0254e390

--- backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccd2f8a5539b00de88e7e3d8652b5f2f99ebc2250fef0116ed580d84e6101d39
-size 5818
+oid sha256:2e34325d5d36261b3f057efbbaa2186bdbfa079293fbe9f895200529c5b57f31
+size 5875

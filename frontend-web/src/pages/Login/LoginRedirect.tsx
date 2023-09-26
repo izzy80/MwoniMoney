f99577598324c841970b51e7e589d59ff73cccb5
박기택 <taekputer@gmail.com>
2023-09-26 17:15:01 +0900

    [S09P22B310-000] config : 라우팅 수정
    Former-commit-id: 80a1b00ba32533c8958963938548c13edb3db8bb

--- frontend-web/src/pages/Login/LoginRedirect.tsx
+++ frontend-web/src/pages/Login/LoginRedirect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36a54df865ed3d1b6d8f26ae599de7fe2dc93320430558f36c3efecacbcb937c
-size 1558
+oid sha256:6400d14c65b197492b76f222a9485f15718c62f18e83e219bbb70ed6dcc2a16c
+size 1542

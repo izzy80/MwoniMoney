a08b929c813245f9bf31a1910ebce9d85dbec60e
tjdfkr011 <tjdfkr011@naver.com>
2023-09-26 12:21:14 +0900

    [S09P22B310-247] feat : 워드클라우드 기능 구현
    Former-commit-id: 2f1ed825bcd180c87d1497f238191082dab8f765

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/WordCloudRepository.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/WordCloudRepository.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:af02d9aa87e691ac5fb3feafc003d035b71055dd9aeea503e6892264fbad1fc8
-size 697
+package com.ntt.mwonimoney.domain.game.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import com.ntt.mwonimoney.domain.game.entity.Word;
+
+public interface WordCloudRepository extends MongoRepository<Word, String> {
+
+	@Query("{word : ?0}")
+	Optional<Word> findWordByWord(String word);
+}

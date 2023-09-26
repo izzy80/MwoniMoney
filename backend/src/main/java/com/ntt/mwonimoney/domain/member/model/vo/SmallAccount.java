c5468f6aef7297b0df4a508ee64faff3e508e432
윤민재 <w124564@naver.com>
2023-09-26 10:51:43 +0900

    [S09P22B310-261] feat : entity 변경
    Former-commit-id: 55e439886b69c205639a2430c86523bc07dbc5cd

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/model/vo/SmallAccount.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/model/vo/SmallAccount.java
@@ -1,23 +1,3 @@
-package com.ntt.mwonimoney.domain.member.model.vo;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class SmallAccount {
-
-	private int goalMoney;
-	private String goalName;
-	private String imageFilename;
-	private byte saveRatio;
-
-	public SmallAccount createVo() {
-		return new SmallAccount(
-			this.getGoalMoney(),
-			this.goalName,
-			this.imageFilename,
-			this.saveRatio
-		);
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4180459e5cb3ba2d4589a595a8477548b9090d72a99957f24f4e21b767304d4c
+size 552

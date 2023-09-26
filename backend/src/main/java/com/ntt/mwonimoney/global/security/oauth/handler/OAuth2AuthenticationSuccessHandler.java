d80363e2c020a41d5d78fef904ba900c6e67d8a6
윤민재 <w124564@naver.com>
2023-09-26 11:20:48 +0900

    [S09P22B310-000] rename : 로그인 테스트를 위해 임시 변경
    Former-commit-id: afcedc40aeeb807eb64523362834c723b3ac8912

--- backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b6b1938e6e55ce42474157985627a5a76dd16171da92cf617968814c5b69731
-size 5803
+oid sha256:ccd2f8a5539b00de88e7e3d8652b5f2f99ebc2250fef0116ed580d84e6101d39
+size 5818

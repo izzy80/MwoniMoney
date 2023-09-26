c751d19f1aa047b89abbf6ccd8591772504e87b4
tjdfkr011 <tjdfkr011@naver.com>
2023-09-26 13:46:55 +0900

    [S09P22B310-000] fix: swagger 시큐리티 open, sql log 중지
    Former-commit-id: 68e6818de538db057b9175c05d29f1a3a2e01c76

--- backend/src/main/java/com/ntt/mwonimoney/global/config/WebSecurityConfig.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db6832606c1623738a8d971873419fd4bfd36db81d38058dc8da7bb6ac28851a
-size 4776
+oid sha256:c4ffee7165b6bb2c7084611a934974f0682eefdd46e3974c742156ae442e0082
+size 4795

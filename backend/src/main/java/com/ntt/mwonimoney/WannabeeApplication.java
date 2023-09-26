07bf61a3362c5333f14c8c9eb747c7d5a4f6bae8
tjdfkr011 <tjdfkr011@naver.com>
2023-09-26 14:53:36 +0900

    [S09P22B310-000] fix: swagger cors 오류 수정 기본값 https로 변경
    Former-commit-id: 86a91973076802101574b2ca5cca3749c8673057

--- backend/src/main/java/com/ntt/mwonimoney/WannabeeApplication.java
+++ backend/src/main/java/com/ntt/mwonimoney/WannabeeApplication.java
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bde3cb2766f7ef1382d6076c74c719e52f600fdbc960ea03b92b441eb6a9b8e5
-size 713
+package com.ntt.mwonimoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication
+public class WannabeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannabeeApplication.class, args);
+	}
+
+}

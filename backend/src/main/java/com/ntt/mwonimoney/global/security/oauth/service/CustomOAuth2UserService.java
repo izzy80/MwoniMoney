c5e170924642aa95c757b12830aa0a7a9831299b
윤민재 <w124564@naver.com>
2023-09-26 10:08:44 +0900

    [S09P22B310-261] feat : 회원 가입 정보 변경
    Former-commit-id: 6061a749e71df588f988bfe08246e96f710be26f

--- backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/service/CustomOAuth2UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80eb574fb4e36ed775e1524cc5fe27ef6d5ab58a0114d87c70bce8644b525489
-size 2582
+oid sha256:064ede0694c7536bfdd81ae91b173d85277e3db118455bc6f1015d697315cc5f
+size 2601

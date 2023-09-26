da34dfb110af7e9e460f142a9892287f36f5ec0a
윤민재 <w124564@naver.com>
2023-09-26 17:52:29 +0900

    [S09P22B310-000] fix : 로그인 오류
    Former-commit-id: 0f236f99e9da6aaf75eb6fd98ced6fd0409217af

--- backend/src/main/java/com/ntt/mwonimoney/global/config/WebSecurityConfig.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:784b8b1d963038307c15b48d817db90ef27dc84694f66a30b54fa2237554593b
-size 4815
+oid sha256:1a2002734f54c2553065c66d8cd658eda8be3cc46e28ab5d079e03ee8842feaa
+size 4839

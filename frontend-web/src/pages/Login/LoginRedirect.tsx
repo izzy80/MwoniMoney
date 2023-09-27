02cd0b7ecc0cb71e01081b958802d6f17cc05921
박기택 <taekputer@gmail.com>
2023-09-27 11:12:25 +0900

    [S09P22B310-000] config : accessToken 넣기
    Former-commit-id: ca61819e00eb060bef75f67e68f1c8772621e141

--- frontend-web/src/pages/Login/LoginRedirect.tsx
+++ frontend-web/src/pages/Login/LoginRedirect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6400d14c65b197492b76f222a9485f15718c62f18e83e219bbb70ed6dcc2a16c
-size 1542
+oid sha256:55ffa70b0372aca7e035e4197382b2a7cf591a1db5093956f274f180f2b2c141
+size 1537

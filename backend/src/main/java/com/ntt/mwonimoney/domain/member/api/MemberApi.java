eecb491f40d5f3651ec0c5dcaf1c1d0e95cb52a7
tjdfkr011 <tjdfkr011@naver.com>
2023-09-28 21:14:53 +0900

    [S09P22B310-198] fix : 멤버 api 수정
    Former-commit-id: 27588d4575ac40562b1a10ef83bc3ada087c581b

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/api/MemberApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/api/MemberApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e464e774991606743c6f7f04a4e1d72b1ba1413fdbb5ddf6d48a411840131984
-size 1916
+oid sha256:20f1af0040f3e5bea0f306bbfa29b17910f8a6de98b615043cadfd99fa186fbf
+size 1940

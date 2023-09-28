9ab3ab54bcbf53ff15c2df5b6ddbb60b98594dcf
tjdfkr011 <tjdfkr011@naver.com>
2023-09-28 20:48:18 +0900

    [S09P22B310-198] feat : 멤버 생성, 조회, 수정 기능 추가
    Former-commit-id: 37c2acbc9382c3869fba9398906e45d692d21565

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Parent.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Parent.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:768e34a350d8d1abc02215833419a0ab1727d939dd0f8d7f337cb6b66c8726e6
-size 1099
+oid sha256:dda34727dd72bd639d4b6dc589dc215ecb1be2257cb057a8726217500338d1a3
+size 1110

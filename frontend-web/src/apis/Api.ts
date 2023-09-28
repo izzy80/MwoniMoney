f49bda5c4e4332b2715bc29c5051fdccbfce70d2
tjdfkr011 <tjdfkr011@naver.com>
2023-09-28 21:18:47 +0900

    [S09P22B310-000] fix : mapping 문제 해결
    Former-commit-id: fef53570b79540eb854731d402c84d6e057801be

--- frontend-web/src/apis/Api.ts
+++ frontend-web/src/apis/Api.ts
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:72c5b826645184cf014972e98f389e163f0783778a590db61fa0ade6405e276a
-size 203
+import axios, { AxiosError } from "axios";
+import { API_BASE_URL } from "./Url";
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-type": "application/json",
+  },
+});

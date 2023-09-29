13a6e9c7c2829d38058d8f9303c9d647bc20ac71
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-29 20:06:18 +0900

    feat : MemberChallenge api 수정
    Former-commit-id: e7b31e2f3fd1e1aac6570ef9336590e66716f50b

--- backend/src/main/java/com/ntt/mwonimoney/domain/challenge/api/ChallengeApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/challenge/api/ChallengeApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afb223d589f8ecb229e459a0b2e64a8f343cdf81145d1f787f4e7fe6767148a2
-size 4682
+oid sha256:a272be9f83abc0f475185f8d396a57976fab6eca1702c6b72ff8d31aba8fa907
+size 5420

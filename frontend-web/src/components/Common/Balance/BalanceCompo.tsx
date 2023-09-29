b9f1a15351014fd0de90bd71ce3a7afe38399988
Jaei <dobbykim0320@gmail.com>
2023-09-29 15:08:34 +0900

    [S09P22B310-242] Design : 마이페이지 수정 중 .. 밸런스게임 디테일창 router연결
    Former-commit-id: 7cd264c2242703a1ffc87e4660ee4c4f1be8665c

--- frontend-web/src/components/Common/Balance/BalanceCompo.tsx
+++ frontend-web/src/components/Common/Balance/BalanceCompo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c71bf5ffcbee04157a7b63471cd48f11c0b80476baa2ce0f254d46dbd56fb6f
-size 4809
+oid sha256:d6e47a2edaf5ff38d4dc10950e1158f1c9c4b57136e4aca51aaa170f6c0f56d0
+size 4971

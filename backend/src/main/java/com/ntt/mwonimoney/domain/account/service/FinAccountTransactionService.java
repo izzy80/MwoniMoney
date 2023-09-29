43053a5f47d6d41c64b3ff16c9cbe10e4fde18ab
aldms8960 <45choi22@gmail.com>
2023-09-29 14:17:59 +0900

    Fix: FinAccountTransactionService 순환참조 수정
    Former-commit-id: 7d9a603615711f831883ffd90aa4268f05d05b91

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/service/FinAccountTransactionService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/service/FinAccountTransactionService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb2362e22c319db8d0d6c6278cb0a13eb13d8e7e0771152c6a7a3250295c23bb
-size 396
+oid sha256:456b8e7ab95d13675a87253673eefd4a87137806029fbad22456e1b8f687a495
+size 317

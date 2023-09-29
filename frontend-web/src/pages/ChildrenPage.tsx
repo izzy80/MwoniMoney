67aa8c3d33d833b4b2de86d64b03e73b35c02ef6
Jaei <dobbykim0320@gmail.com>
2023-09-29 15:20:22 +0900

    [S09P22B310-242] Feat : 임시
    Former-commit-id: ab6e6f26ae63976f3071ebc9ac43abe4a26874ac

--- frontend-web/src/pages/ChildrenPage.tsx
+++ frontend-web/src/pages/ChildrenPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9849e47eefc929367398c70922975afac69a7fbdb4084ed6f8ee2a2182072f9c
-size 2180
+oid sha256:050dc19fdd1101d8c58b860e8eea45e06855f24e4c0d37454c970adece4b1ef7
+size 2165

f2cd9b5d75c7203c4597e4363d08cf58ae05221c
tjdfkr011 <tjdfkr011@naver.com>
2023-09-29 16:20:09 +0900

    [S09P22B310-000] feat : 밸런스게임 로직 변경
    Former-commit-id: 2509fcadf7d9c464fc23b23a36c6262e4750aff9

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/entity/BalanceGame.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/entity/BalanceGame.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91824925e8fa1026e86127c4beb15aa502358304ba3cce7a4e06b5ba5563c4dd
-size 1900
+oid sha256:ddbcb72eb2a719aeb78377b84cb235323b2b81c8d9f01694ad10d22e46c78833
+size 1946

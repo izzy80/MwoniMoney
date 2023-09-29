c7dd6938bb9702002cacc3a9a8822ee008f6ad0b
박기택 <taekputer@gmail.com>
2023-09-29 17:32:13 +0900

    [S09P22B310-254] : feat : 밸런스 게임 axios 연결 v1
    Former-commit-id: ccc597c5268875bcdd4fbf06b27da87b4b2931af

--- frontend-web/src/pages/Balance.tsx
+++ frontend-web/src/pages/Balance.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8224cb94758e197ae754a67e0e8d4f752285527687f245018e1095645b35b25
-size 2686
+oid sha256:8dc6bfab6ff76682ec708c13876d19f8b7281eed7f4d35a937cfce68cefd22f7
+size 4546

b14abf3b96ee81a008c219e098782aa3966223ec
tjdfkr011 <tjdfkr011@naver.com>
2023-09-29 14:37:32 +0900

    [S09P22B310-000] fix : 밸런스게임 오류 수정
    Former-commit-id: 73613aef9bdeb052cc5c1b50c73dc7c56c3e76a5

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/CustomBalanceGameRepositoryImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/CustomBalanceGameRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0fff4d40876a213ba6564a3762e6e94775f1d32e39f5157f0bee2e1cd8d7868
-size 3385
+oid sha256:f7bec79c5c92c9771f0db29e3d38b5a9231571471b9483fbb1166c36a0683ff6
+size 3314

5156dbc17641ac548cd84ab632876035dab59aa5
tjdfkr011 <tjdfkr011@naver.com>
2023-09-29 21:34:33 +0900

    [S09P22B310-000] fix : jwtChainFilter 로직 변경
    Former-commit-id: 1b14c0ce3cdb23a4c05db4824df10b667227fbda

--- backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/exception/UnAuthorizationException.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/oauth/exception/UnAuthorizationException.java
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5a67d5986d48a5c113ff85f9a44e7fc61ccd53385f343ec96328a4ec92549142
-size 201
+package com.ntt.mwonimoney.global.security.oauth.exception;
+
+public class UnAuthorizationException extends RuntimeException {
+
+	public UnAuthorizationException(String message) {
+		super(message);
+	}
+}

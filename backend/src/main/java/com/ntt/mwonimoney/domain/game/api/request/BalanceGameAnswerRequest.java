2ebe4ae3e6cc5f33fa2c77abb1f74caa5c54a738
tjdfkr011 <tjdfkr011@naver.com>
2023-09-29 14:51:30 +0900

    [S09P22B310-000] fix : 밸런스게임 오류 수정
    Former-commit-id: 6066d3f99342367ff891124d2a5fb8c29acd0e0a

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/api/request/BalanceGameAnswerRequest.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/api/request/BalanceGameAnswerRequest.java
@@ -1,13 +1,3 @@
-package com.ntt.mwonimoney.domain.game.api.request;
-
-import com.ntt.mwonimoney.domain.game.model.vo.BalanceGameAnswer;
-
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class BalanceGameAnswerRequest {
-
-	private BalanceGameAnswer selectAnswer;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5260e299494d97e9e15400daf8f3c1287b75cb22df3ac3de36e4ab94b0d80393
+size 369
--- backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/CustomBalanceGameRepositoryImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/CustomBalanceGameRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcb73c21d9783005db79249d44961841fb74b2ca89a88bc2eb99608d8af1abbf
-size 3382
+oid sha256:8f8d23a2cccb90165a5edde7d7b37659c7e0485e1021eca713f057926a6298a7
+size 3539

b598a79f7b07f8b33bad6329129bbe566a31d0d1
tjdfkr011 <tjdfkr011@naver.com>
2023-09-29 14:54:15 +0900

    [S09P22B310-000] fix : 밸런스게임 오류 수정
    Former-commit-id: fe701222feed811db7880f3e7dcbb8e52344f454

--- backend/src/main/java/com/ntt/mwonimoney/global/security/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/jwt/JwtTokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c3e2ea729713b55a21a4565dc196b1a6b02a44f5127bf1baeaf6165f5c9c4e6
-size 4529
+oid sha256:fd5a05b61c3d535eaebb742080ac8d2b1180afd551a57f7fe00b3b8619bd173a
+size 4530

e6f441c0a806f4a103ba2e61b27330d933088bbf
tjdfkr011 <tjdfkr011@naver.com>
2023-10-01 03:51:37 +0900

    [S09P22B310-000] feat : 오타수정 및 디버깅을 쉽게하기 위한 jwtFilter 리펙토링
    Former-commit-id: df7051d8d51c5b2218e0b3ef71b2980c0350092b

--- admin/chatbot/urls.py
+++ admin/chatbot/urls.py
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:457aee9fb812591b00c35a1bfc249311518ad5f5419c99b2e6d4f78e955fca86
-size 102
+from django.urls import path
+from . import views;
+
+urlpatterns = [
+    path('answer', views.answer)
+]
---
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/jwt/exception/ExpiredTokenException.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7cd44fd2919d6c823e3e896318221b04a266edb7accbf1a0dbb8fe1c7e3cb0e
+size 192

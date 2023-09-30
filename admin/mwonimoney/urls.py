2e8db388962eda23bf6e2fe86f0d00b6072ad2ec
윤민재 <w124564@naver.com>
2023-09-30 20:38:36 +0900

    [S09P22B310-255] fix : url 변경
    Former-commit-id: 6f72f185fa77f6b4c807dbb5bdad82588bc5d5d7

--- admin/mwonimoney/urls.py
+++ admin/mwonimoney/urls.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:589947a65b739b51b6f7a1043b739046049067ef9ebe5e590dda762d9bbe2a93
-size 806
+oid sha256:f0eb7805cd22e837e12bd43a83e992a45403bcca81a51190d1789cf94fee0e23
+size 813

a27d31366d7dfe780d2921e42149c7077275d096
Jaei <dobbykim0320@gmail.com>
2023-09-30 16:22:49 +0900

    [S09P22B310-267] Feat : 역할정하기 페이지 새로 제작 및 axios post
    Former-commit-id: 719559d5d0664161c453605532388d37c22edd9b

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:469ffe186b86b73e7db718655cce6676e7dee1da50be79878bbfdd11bab2f372
-size 5780
+oid sha256:a8f3ec8d67d7e464fdb83d13ab35384dcb799341fd5a9765b96ac303c2e3ef4c
+size 5913

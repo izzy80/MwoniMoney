bb4d38609b0c5dbbf3394fe60454b0a94b1fbed2
tjdfkr011 <tjdfkr011@naver.com>
2023-09-30 12:56:49 +0900

    [S09P22B310-000] feature : 멤버 정보 변경 이름 추가
    Former-commit-id: f5f079d6797701ba4dd0f72e354460a968d0d750

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Member.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1cdb28f10119093118aef54ab65e7f4aab6f56c0403a1e15f651cc1b153d8d2
-size 3895
+oid sha256:d21ce4c8288235ce6e8321b5db9cea4647c2efd960cfef6454d42c24df1ba066
+size 3964

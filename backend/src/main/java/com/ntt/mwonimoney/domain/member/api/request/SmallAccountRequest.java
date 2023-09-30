93fcd5c28dbc2eb3dc1a2cf1fddef02847e655c3
윤민재 <w124564@naver.com>
2023-09-30 20:07:18 +0900

    [S09P22B310-255] feat : 챗봇 기능 개발
    Former-commit-id: c49de003ff12841655d1d2b9d63b5a2b50f3ec09

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/api/request/SmallAccountRequest.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/api/request/SmallAccountRequest.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:79a842a84db1ebbb5e554dbebcf58a1eff024fda8d7e8d927a11bd18c3f7d1d5
-size 225
+package com.ntt.mwonimoney.domain.member.api.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SmallAccountRequest {
+	private int goalMoney;
+	private String goalName;
+	private int saveRatio;
+}

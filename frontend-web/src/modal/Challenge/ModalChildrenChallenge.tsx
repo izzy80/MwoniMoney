1f2337f0a620de2ba7c540eeca2087496e9de541
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-30 16:53:58 +0900

    feat: 자녀 챌린지 post axios
    Former-commit-id: 709aba8627f25585a3d2b652b3fc2cafeb3779f3

--- frontend-web/src/modal/Challenge/ModalChildrenChallenge.tsx
+++ frontend-web/src/modal/Challenge/ModalChildrenChallenge.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:295d3c200a11c7f1dd58b83eb2a2c4ad6278c2dab9791429cc33396cfb505289
-size 3218
+oid sha256:b999812b10f85f0b54faed79c6e71765c629137695a617c4061d3f46fff0c695
+size 5112

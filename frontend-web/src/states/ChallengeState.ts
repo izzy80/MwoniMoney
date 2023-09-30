ee1ca585a4bd0459a508ed8882787b97f902d196
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-30 15:27:23 +0900

    feat: challenge modal 수정
    Former-commit-id: 001212f091777bf06ea153e395a3d078a2b1da8d

--- frontend-web/src/states/ChallengeState.ts
+++ frontend-web/src/states/ChallengeState.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3ab676247375fe6cedb9bbe224d5fa39bdf7fe72c087aa9860cb9001ebdaa0b
-size 524
+oid sha256:6189b833a3b6a6ae241740406f34810e6288e7f8450c84fbd1b5f5011015d0c2
+size 665

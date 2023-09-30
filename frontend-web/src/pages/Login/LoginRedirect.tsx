dd12017eae95456b3bb9d61710439ed3f5f087a0
박기택 <taekputer@gmail.com>
2023-09-30 14:09:45 +0900

    [S09P22B310-000] feat : userState수정 및 qr uuid
    Former-commit-id: 7367b29aebce70adda86afa66bd30eb697868873

--- frontend-web/src/pages/Login/LoginRedirect.tsx
+++ frontend-web/src/pages/Login/LoginRedirect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1466cc43784310daf92701bd0c1e8665941791dbd9b39aa69d3d6e1bf833090c
-size 2898
+oid sha256:3f69e61d41caeee8a7a360322789742a0135895426233d6cc3cb7c2e76aac39d
+size 2899
--- frontend-web/src/pages/Qr.tsx
+++ frontend-web/src/pages/Qr.tsx
@@ -1,16 +1,3 @@
-import React from "react";
-import { useState } from "react";
-import QRCode from "qrcode.react";
-
-function Qr() {
-  const parentsUuid = localStorage.getItem("");
-  const qrLink = "https://j9b310.p.ssafy.io/api/v1/children/${parentsUuid}";
-
-  return (
-    <div>
-      <QRCode value={qrLink} />{" "}
-    </div>
-  );
-}
-
-export default Qr;
+version https://git-lfs.github.com/spec/v1
+oid sha256:c751d321f1c871ecbd990c6f5cd1c67a4f92cba84747c130e9b8ccf4f18fafb8
+size 1147

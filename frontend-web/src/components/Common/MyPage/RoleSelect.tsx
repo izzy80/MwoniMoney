580f7e0bf5cb23db51c2a76bb8169e10261e6f34
Jaei <dobbykim0320@gmail.com>
2023-09-30 19:18:05 +0900

    [S09P22B310-250] Feat : memberRole설정 후myPage로 가도록 설정
    Former-commit-id: 8f840d8b0c7f4903a047092ab270c230a4dba253

--- frontend-web/src/components/Common/MyPage/RoleSelect.tsx
+++ frontend-web/src/components/Common/MyPage/RoleSelect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:621b55bd63b3ade0c2c8a0964bfed99870400234e2a534c031d3b3a03a9eedfc
-size 3689
+oid sha256:8153f90a1aaae9b912f77141a3599ec821d0f18b9701092863dc8ae1228fa31c
+size 3686

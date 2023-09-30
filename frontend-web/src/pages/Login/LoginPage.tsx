96f71e734b6251a2a64f77f6a876b7804c5d4c75
박기택 <taekputer@gmail.com>
2023-09-30 14:13:52 +0900

    [S09P22B310-000] feat : google login
    Former-commit-id: 1cde5de413d5de76605d137c86316f2468942e64

--- frontend-web/src/pages/Login/LoginPage.tsx
+++ frontend-web/src/pages/Login/LoginPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69fd402d28c7c26e8b2615c6ed4eab893706c844ab15a53cb7862d8513538e9a
-size 2099
+oid sha256:5a94aab47caab05eb774ae66e47a8036bd90d7641deae5aa16f4a71681643e3c
+size 2497
--- frontend-web/src/pages/Qr.tsx
+++ frontend-web/src/pages/Qr.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c751d321f1c871ecbd990c6f5cd1c67a4f92cba84747c130e9b8ccf4f18fafb8
-size 1147
+oid sha256:de8a801134c61e1d5335ab6b75375dfcb08b86cf6602b3efcd11e4cd53664144
+size 1146

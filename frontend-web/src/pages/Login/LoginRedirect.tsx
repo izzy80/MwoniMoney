b60b934202eaa93419c7479ce5123186ddc91831
박기택 <taekputer@gmail.com>
2023-09-30 16:01:42 +0900

    [S09P22B310-000] feat : qr login 수정
    Former-commit-id: 39c2ea078d35396722db56b195fd265b65be7e17

--- frontend-web/src/pages/Login/LoginRedirect.tsx
+++ frontend-web/src/pages/Login/LoginRedirect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f69e61d41caeee8a7a360322789742a0135895426233d6cc3cb7c2e76aac39d
-size 2899
+oid sha256:edbd3a223bf911b54bc88c630d411aa3856545947beaf4e91d6649d9f8c806c0
+size 2818

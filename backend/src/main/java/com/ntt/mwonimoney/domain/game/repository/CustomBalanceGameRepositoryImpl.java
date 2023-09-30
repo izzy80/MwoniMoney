22d4d0c9e8d5f1a60b506e8cf891d856db16baaa
tjdfkr011 <tjdfkr011@naver.com>
2023-09-30 17:17:37 +0900

    [S09P22B310-000] feat : 벨런스게임 뉴스기능 추가
    Former-commit-id: 2d2be7422262798747204e4da42370f7ff5cb888

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/CustomBalanceGameRepositoryImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/CustomBalanceGameRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6877671e2c8f6227f76546baae086b6dca242036acd48151e7e8993c72210ca3
-size 4396
+oid sha256:706b9d941a402b8e3e06c736fe7fbf18db68ef396e7cd91dac03638271f9d7ca
+size 4442

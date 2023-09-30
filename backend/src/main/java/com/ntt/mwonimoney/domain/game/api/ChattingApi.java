82d3712f02acd2a97337759cd9d8cd2ac9bca898
tjdfkr011 <tjdfkr011@naver.com>
2023-10-01 02:12:05 +0900

    [S09P22B310-000] feat : 채팅 로그 추가
    Former-commit-id: fbb79df44f37b166ee3402c8b63953ec98377968

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/api/ChattingApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/api/ChattingApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d95ca7472763f66c9dd349e06153e9b7b5d74eb5f387c8e6248ec1363dafd07a
-size 1649
+oid sha256:64ded3746a4fe1b946b2c9cd3f8ad6946297437a4280e21f98fff04a086da291
+size 1807

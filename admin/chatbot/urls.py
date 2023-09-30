883c5a05c8a6cab2a2a8b586c63e47d22493b12f
윤민재 <w124564@naver.com>
2023-09-30 19:56:33 +0900

    [S09P22B310-255] feat : 챗봇 기능 개발
    Former-commit-id: 5bc45452fa467520908cee978d0465a2614de0f9

--- admin/chatbot/urls.py
+++ admin/chatbot/urls.py
@@ -1,1 +1,3 @@
-app_name = "chatbot"
+version https://git-lfs.github.com/spec/v1
+oid sha256:457aee9fb812591b00c35a1bfc249311518ad5f5419c99b2e6d4f78e955fca86
+size 102
--- admin/chatbot/views.py
+++ admin/chatbot/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6c3d8e83ec131a793b720d1fb6fb6a04034d6879eaf114218e5e58ded3cd8e8
+size 2537

2a6495f67b6f1cbad4cd1e4eceabfe804ccced9b
tjdfkr011 <tjdfkr011@naver.com>
2023-10-01 13:14:58 +0900

    [S09P22B310-000] feat : 워드클라우드 새로고침 시간 3분으로 늘리고, 형태소 분석 좀더 정확하게 해봄
    Former-commit-id: 7af9fff324e560e21a2c4ae05fe412fcaabcb5b3

--- frontend-web/src/components/Chatting/WordCloud.tsx
+++ frontend-web/src/components/Chatting/WordCloud.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d684584e06af3a00156f0cc0812c7f1608a875106a8cd55799fe2dd6e76009b9
-size 893
+oid sha256:b3603f7802eb98d81efcf26f4d3a3c19f7e8d75daef79b9e2a0977d374f4e8b5
+size 894

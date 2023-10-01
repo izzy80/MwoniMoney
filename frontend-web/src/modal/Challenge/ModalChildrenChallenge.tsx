793d98356d4dbf03c6dc327523fe5a4bdc9b4aa9
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-01 22:22:40 +0900

    [S09P22B310-000] feat: challenge 부모 patch, post axios
    Former-commit-id: 7c28701d418bff360f918af403ed121e7374e7a6

--- frontend-web/src/modal/Challenge/ModalChildrenChallenge.tsx
+++ frontend-web/src/modal/Challenge/ModalChildrenChallenge.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5958af02e26bde69e506da07a3b7e422d388dc88448a8052bf612d9dd00928c
-size 5297
+oid sha256:98210249e3b420a8d2505205462b6a9df0fd3e5035bab4bd4b213c77a3af6051
+size 5878

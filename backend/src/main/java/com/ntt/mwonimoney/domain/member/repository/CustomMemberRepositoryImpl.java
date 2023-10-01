5cf6805826893eba5939416b29857f88643eff06
tjdfkr011 <tjdfkr011@naver.com>
2023-10-01 14:59:18 +0900

    [S09P22B310-000] feat : 역할 안바뀌는 문제 해결
    Former-commit-id: c8a38114bbf42f2879c4e8fd49c074fa98007f60

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/repository/CustomMemberRepositoryImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/repository/CustomMemberRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a03d8b2855421f4fddfbc3c9c3f1461be9b5d4562dc66d5765bf16d57b3ef74
-size 3993
+oid sha256:b4a6d1e797ddeb8dc926b9df2fc973ffed5bb186f65490612c518bbf8eba2870
+size 3991

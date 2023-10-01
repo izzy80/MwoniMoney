f3d6dc38db69afef7ae64f4c9e64fe0ad7cb15b8
tjdfkr011 <tjdfkr011@naver.com>
2023-10-01 14:53:16 +0900

    [S09P22B310-000] feat : 역할 바꾸고 로그인 풀리는 문제 해결
    Former-commit-id: c9eb615c58b72d6de27399977286aa9aca3bb118

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/repository/CustomMemberRepositoryImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/repository/CustomMemberRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91f1cb1f9f4ae7e7c33c9b530b8cdc7f98c72385f27bf594cf69a39d737d6412
-size 3872
+oid sha256:7a03d8b2855421f4fddfbc3c9c3f1461be9b5d4562dc66d5765bf16d57b3ef74
+size 3993

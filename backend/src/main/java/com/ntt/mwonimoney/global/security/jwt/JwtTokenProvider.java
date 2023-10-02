d04a6818a6829774cde1175f69b196a593fdb3a8
tjdfkr011 <tjdfkr011@naver.com>
2023-10-02 19:17:14 +0900

    [S09P22B310-000] feat : refreshToken getMemberUUID 로직 수정
    Former-commit-id: 3142e28786218c7a8446b7b26fbfac139920f5d0

--- backend/src/main/java/com/ntt/mwonimoney/global/security/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/jwt/JwtTokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49bcb59e20e204affe0b2b1d89e825a49bd56f15083672395f7189500ead3584
-size 4724
+oid sha256:6414e92198c61752c736c7e0866220b459b8b8f643c1f1153ae45dabe9e50f8e
+size 4755

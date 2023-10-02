99bb87c711ce7cbc8d920d3363e1309f1da2e805
tjdfkr011 <tjdfkr011@naver.com>
2023-10-02 20:30:41 +0900

    [S09P22B310-000] feat : SmallAccount 이미 URL 포함하여 RETURN
    Former-commit-id: 828d2fe4fff7921556a175d319bf45f5158fc69e

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Child.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Child.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57d31ac008cd5b748523783499526344c1051485d73bba3bc35a22aeeebba977
-size 2835
+oid sha256:b8ad3adf0ca3f1f85729b1755b6c395f3653808cd90f451fef80874e952b579c
+size 2814

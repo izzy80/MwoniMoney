b6c421b52cfd3cee91623db9df376d0185f4458f
tjdfkr011 <tjdfkr011@naver.com>
2023-10-02 15:59:24 +0900

    [S09P22B310-000] fix : refresh token 설정
    Former-commit-id: 1dc919b833ecf3005166c2703e7ace80a5056ee5

--- frontend-web/src/apis/Api.ts
+++ frontend-web/src/apis/Api.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dce20ac63bd74579a7165a498b25b1e48aa78fb63a5e9394cd99c8be50709962
-size 664
+oid sha256:5a8d356ff8566faa75daca162073b7a7a104b73467140c3d4c1e78ad031dc9b4
+size 749

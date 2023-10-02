c9a51b3dbec4b4115f98ce3aee27c0b8b5c77e29
tjdfkr011 <tjdfkr011@naver.com>
2023-10-02 19:12:51 +0900

    [S09P22B310-000] feat : S3 URL포함시켜서 리턴
    Former-commit-id: 4e306af2db9171cb4138ae249dad6aeb8c64f606

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/model/vo/SmallAccount.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/model/vo/SmallAccount.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59c47befdc70cde12640658f1ced755471d841238d2d5f1a9fb3ae5b6bfe217e
-size 746
+oid sha256:26de00c8836e5b2ece95a90d2e88efc781f52e27f4f6691cabb64e7bfdabe772
+size 893

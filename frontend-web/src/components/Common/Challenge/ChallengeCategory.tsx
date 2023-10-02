ca4a6ee153d34c597766ffdf276385aef843bf5f
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-02 22:37:37 +0900

    [S09P22B310-000] design : 챌린지 버튼 비활/활성화
    Former-commit-id: 178279be7dacb9d981f68bbf67a438b194ce621f

--- frontend-web/src/components/Common/Challenge/ChallengeCategory.tsx
+++ frontend-web/src/components/Common/Challenge/ChallengeCategory.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2c28316aa85fc4affc1105eb4be0152f19e2ed84393e0666471e96056009115
-size 2304
+oid sha256:6ed309d41c035d9a96605d550d18ecb166b1d529e347bdc9b47cc239b0f274d4
+size 3265

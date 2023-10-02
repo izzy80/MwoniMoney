dee095bd6f714c69ac70650b50cc7d1bb9be7fd3
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-02 15:30:25 +0900

    [S09P22B310-000] fix : 챌리지 삭제 후 완료 누르면 에러 뜨는 거 처리
    Former-commit-id: 427ea57a751435d9721f2a06e21dde81fa6e8ee6

--- frontend-web/src/components/Common/Challenge/ChallengeList.tsx
+++ frontend-web/src/components/Common/Challenge/ChallengeList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32fad7bda5af83b24bc0078374183af3ef86a9781694cb9e5466c893b198801c
-size 8916
+oid sha256:f2fd2c0f8f39f2c74d32e4940ab30f8f7b97fb2cc73e7438874570d6ee57605d
+size 9118

4195f5e78f5962d06e5dfc051ed7acb7bcfa4aba
Jaei <dobbykim0320@gmail.com>
2023-10-02 19:49:51 +0900

    임시저장22
    Former-commit-id: dc9389ca24e10f4401243b82dd55461dea1c14be

--- frontend-web/src/components/Children/GoalCreate.tsx
+++ frontend-web/src/components/Children/GoalCreate.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdb9cdf42e04f25a89922e9ffb261dc3914764460d7dbda5920c58480825530d
-size 5657
+oid sha256:5eb05a17b9b0e40947790a38202d05b2b447127d99aa5e1f8e0aafae10018c15
+size 5567

1d08ea8bf0101b20075af86b9481d9f1a6fc8179
윤민재 <w124564@naver.com>
2023-10-02 19:34:22 +0900

    [S09P22B310-000] fix : refresh token 문제 해결
    Former-commit-id: 74c3f19219b00454791744723108b587bd5e3688

--- backend/src/main/java/com/ntt/mwonimoney/global/security/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/ntt/mwonimoney/global/security/jwt/JwtTokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49bcb59e20e204affe0b2b1d89e825a49bd56f15083672395f7189500ead3584
-size 4724
+oid sha256:8ee555848289a8114137a3cb289f9af45f11263f45f5c096a48ed8d2ea3e0809
+size 4721

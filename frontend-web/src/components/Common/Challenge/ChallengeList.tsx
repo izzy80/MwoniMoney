071c92a34ab55d755c86c3bd0269199b0f679b03
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-02 14:17:04 +0900

    [S09P22B310-000] feat : challenge list button axios 에러 처리
    Former-commit-id: 722c780ce9bcb8a0246abd7a843b44e3f6b71ff3

--- frontend-web/src/components/Common/Challenge/ChallengeList.tsx
+++ frontend-web/src/components/Common/Challenge/ChallengeList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83515bcd4ec6864866992ec729fee37821a34f0ca7fa7602d770cc9ec75a5c44
-size 7868
+oid sha256:2b4af3cf287a74f49c01db2c2c738da4e5e1ea0096580059106f78945be48332
+size 8583

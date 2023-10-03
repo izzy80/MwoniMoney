68039ee5e27bd5b99becf34bae9595f320146efa
aldms8960 <45choi22@gmail.com>
2023-10-04 00:39:32 +0900

    Fix: loan balance 수정
    Former-commit-id: 98c814ce11decc8444fa9e073f1e37a6f951e3f4

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/FinAccountTransaction.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/FinAccountTransaction.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e50c2877583329c9225ba26bd0bcb7195d852d77bfd30456924bdc0ac324076
-size 1361
+oid sha256:f264891cd3713dab95bea7c6f344e7446de3f940cc291d50502653068a4e690d
+size 1358
--- backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/Loan.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/Loan.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:415845aa4ed4c2958981c171498eb6edf9e82c7f78a825ec7866b8eba2e9568b
-size 1693
+oid sha256:2a6125421a40f03e2d066e5fad43da8cec4d6bca9efc31e855e42e910dde9249
+size 1696

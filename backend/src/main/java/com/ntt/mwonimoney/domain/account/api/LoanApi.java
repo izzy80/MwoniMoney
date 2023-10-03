647109d1f1d2ffeceb9e61a81d0e5abbf97beb89
aldms8960 <45choi22@gmail.com>
2023-10-03 16:35:06 +0900

    Fix: createLoan 수정
    Former-commit-id: 694e2c76ff1d5ba047314cbfc76c54927b3e2cd6

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/api/LoanApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/api/LoanApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a45e51dc026741e7961f1d925f3f4bb199098387131763a3f5a04eb9fa578d8
-size 6424
+oid sha256:5b6ea997f3aace874f2329aaa5d26866405f23467e8b32c46739e2ceb8aa94f4
+size 6523

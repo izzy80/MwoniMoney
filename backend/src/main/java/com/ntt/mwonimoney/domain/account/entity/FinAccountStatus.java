2bc0e230dc42e324f0183269a03befabdf444c31
윤민재 <w124564@naver.com>
2023-10-03 23:16:30 +0900

    [S09P22B310-259] feat : 챌린지 알림 기능 추가
    Former-commit-id: 4c84bb315adffd07e151757c37521ccaa3d8b256

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/FinAccountStatus.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/FinAccountStatus.java
@@ -1,5 +1,3 @@
-package com.ntt.mwonimoney.domain.account.entity;
-
-public enum FinAccountStatus {
-    ACTIVATE, DEACTIVATE
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc18605c570bca96951e0705c436e8f018b1b476b91db697a50755679d643f83
+size 109

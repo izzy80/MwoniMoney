33f9f6e81369e928f50fbcd0dd623327f036b926
aldms8960 <45choi22@gmail.com>
2023-10-03 22:03:03 +0900

    Fix: loan 수정
    Former-commit-id: 0a86dfb58b0acf0e355d8f8a638d5afc86c00900

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/LoanRepository.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/LoanRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b4885be49ddea0873f1f2847c69c0ec90583857522b3e39e7fd7c2a076d6ef6
-size 798
+oid sha256:01abb149b2faf2e3d6f83572009dd19451e691085e7a78b47b7b6d66a2a499f0
+size 855

f5ad2e8b8c8963430827fe5fc8f859f91e8f1f5e
aldms8960 <45choi22@gmail.com>
2023-10-03 23:54:22 +0900

    Fix: small-account 저장 수정
    Former-commit-id: 7b32943e2ad58f3808dd39a83aa13a863f68c965

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/FinAccount.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/FinAccount.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3af6def34d3917b94d1584316c80a26ca1e60b2f17312eaeb0e0ddf4b9c4ba71
-size 1949
+oid sha256:c54c888db954f55c695d9c5a23c2a14236a3efbfa73fadc5733825211a575453
+size 1988

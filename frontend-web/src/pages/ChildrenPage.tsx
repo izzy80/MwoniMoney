768aa47d783fad4976b0d5d4ef37a5f65f1c29d7
박기택 <taekputer@gmail.com>
2023-10-03 15:46:17 +0900

    [S09P22B310-000] feat : 메인페이지 밸런스게임 연결
    Former-commit-id: 207be9de66248815bdae35ea017a46db5e5ecfc2

--- frontend-web/src/pages/ChildrenPage.tsx
+++ frontend-web/src/pages/ChildrenPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b1aeb37a7ca2dce03302d55a33747e2a5edc92f1a012d4c244d7c4ca3ab572e
-size 2814
+oid sha256:1515bf177561b33f19b3e107ef83c3652b542c63bb7718da5240639623faa86c
+size 4605

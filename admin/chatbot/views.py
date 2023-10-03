b2a393f397073ef4a808cc12d7d11e64cbdc4fcb
윤민재 <w124564@naver.com>
2023-10-03 18:32:37 +0900

    [S09P22B310-000] feat : 챗봇 변경
    Former-commit-id: 8e745a633123c2015adf2e8ee82df2e17e45e5ea

--- admin/chatbot/views.py
+++ admin/chatbot/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4ff54e00cbf301edf95c8f9d88ae212078625da08f683cebe23c92801927880
-size 2506
+oid sha256:a431261bb4ed0617ac4abeaa9ccfbf9e907fbccf8eb25a972c595736f240c063
+size 2507

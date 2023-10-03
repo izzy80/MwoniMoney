181c0b895f06ed566e3b2825c195c10cf44d0fe2
박기택 <taekputer@gmail.com>
2023-10-04 02:35:55 +0900

    [S09P22B310-000] feat : 거래 내역 조회
    Former-commit-id: be2df512f3b1c7171fd55108d0c1c683400b16e1

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/api/FinAccountApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/api/FinAccountApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:902d84e6df2bcf21d97e50828ddc3f5fac0b14ecf909ba39902be3cfcf4acf78
-size 15018
+oid sha256:b598a0d3487fb881ee81c315f964166607418198b1bd4a501f7f0122985a552b
+size 16076

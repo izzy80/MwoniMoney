9af18fd4a07839786ff2d86d6f8efe73b28bac53
박기택 <taekputer@gmail.com>
2023-10-03 20:15:44 +0900

    [S09P22B310-000] feat : 챗봇 추가
    Former-commit-id: 59ff7f7188519744ae58dfbe25c88db7392a58fc

--- frontend-web/src/components/Common/Main/ChatbotLink.tsx
+++ frontend-web/src/components/Common/Main/ChatbotLink.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3f4e62ef227bfa1debfec1f1de6237d892bc92b7d9a3c9e786934b0a51133b6
-size 1050
+oid sha256:9350272b1055a4ed095e14e8cafd73d933bb85787a1dad460181d1cc23c98088
+size 1065

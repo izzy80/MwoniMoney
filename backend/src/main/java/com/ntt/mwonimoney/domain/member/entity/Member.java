ef12005046ceeb784210072fff00841d4aed6134
윤민재 <w124564@naver.com>
2023-10-03 16:27:18 +0900

    [S09P22B310-259] feat : fcm 설정 추가
    Former-commit-id: b1a0fec518896694b97a54afc96c083bbdbb13e0

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Member.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9081350349636c6cbb4b2644ec3b36def2ad1513fc6d269aca75ec3ca238fd33
-size 3169
+oid sha256:b75b2af65fabc990d2e85fe5abf518a0f1108033a88bb4979de1370a101461a9
+size 3360

1bd8283ab3d3a2b15d6844698d69a0bcc9305071
aldms8960 <45choi22@gmail.com>
2023-10-03 16:15:09 +0900

    Fix: api/v1/loans 수정
    Former-commit-id: c0adde38af0c7e67e733e7ab67f812a5c76f726d

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/api/response/NHOpenVirtualAccountResponse.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/api/response/NHOpenVirtualAccountResponse.java
@@ -1,16 +1,3 @@
-package com.ntt.mwonimoney.domain.account.api.response;
-
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-public class NHOpenVirtualAccountResponse {
-    private NHApiResponseHeader responseHeader;
-    private String Dpnm;
-    private String VractPrtn;
-    private String Vran;
-    private int IsncRmngCnt;
-    private int IsncFncnt;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7fc145a3cdcc71982fd1dd1e9200e76e6687218e1fc781a31f0392085b7a2fb5
+size 618

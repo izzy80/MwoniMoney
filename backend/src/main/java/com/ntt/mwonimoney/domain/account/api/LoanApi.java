6991a807d200cbaccc8eb0602e59a05b08799966
aldms8960 <45choi22@gmail.com>
2023-10-04 01:26:26 +0900

    Fix: repayloan 수정
    Former-commit-id: 35f146761a13a108b2a3bd968fe9ae16529bdca0

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/api/LoanApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/api/LoanApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83d2bc7105e27b9def75f57ee8bc61cb23ccc2c84cac3613b63f519247cb58d6
-size 5374
+oid sha256:394a4be4fe7a81f504e15b323de0670a277a21a6244157acd754cb93c2953d46
+size 5372

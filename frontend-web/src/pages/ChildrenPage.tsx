640ba3f772d301ce350ebaf4704a9e243bb81254
박기택 <taekputer@gmail.com>
2023-10-03 15:23:14 +0900

    [S09P22B310-000] feat : 용돈기입장 코드 변경
    Former-commit-id: d93bf282f6ff5d53e56a459aa0be5155e7d0d08c

--- frontend-web/src/pages/ChildrenPage.tsx
+++ frontend-web/src/pages/ChildrenPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07c4a38bbedef2e772b09eb5db6b1281cd8dd33b21f4f6a28de71c246f8cc46b
-size 2571
+oid sha256:0b1aeb37a7ca2dce03302d55a33747e2a5edc92f1a012d4c244d7c4ca3ab572e
+size 2814

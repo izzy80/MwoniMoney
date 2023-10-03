23839d2addaa082f0924c8209396c1cf62934f9b
tjdfkr011 <tjdfkr011@naver.com>
2023-10-03 21:48:20 +0900

    [S09P22B310-000] fix : 서버 업로드 가능 최대 크기 50메가로 변경. 나의 지갑을 지켜줘..
    Former-commit-id: d59b72e1dc43369432896b18ddb0c8f87179b5fc

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/model/dto/LoanListRequestStatus.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/model/dto/LoanListRequestStatus.java
@@ -1,5 +1,3 @@
-package com.ntt.mwonimoney.domain.account.model.dto;
-
-public enum LoanListRequestStatus {
-    GENERAL, APPROVAL, REJECTION, WATING;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a4602152a85fc1136fb2373ee086deef0cf7578e5efdb0ef83010e6dfcfec63
+size 134

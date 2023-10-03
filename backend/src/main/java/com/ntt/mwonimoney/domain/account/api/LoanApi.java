a59f6bf7354ecdd39a2df290f9cee6222c44580b
aldms8960 <45choi22@gmail.com>
2023-10-03 17:38:40 +0900

    Fix: loan 조회 url 수정
    Former-commit-id: 1551f608d8787004ea5ae8d5c276d8f643b1f550

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/api/LoanApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/api/LoanApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df52043f32ed80bfaa81c7803491cc15ce23b52c9686833c3e2d96a7ef7e26b7
-size 6608
+oid sha256:248264a8259d90baa435beec84ca491f2eb51c9182f38c1ce6d12620334e5c9e
+size 6581

25809ab1050f545ed43e72ed79e21c7bc9354833
aldms8960 <45choi22@gmail.com>
2023-10-03 21:58:31 +0900

    Fix: loan api 수정
    Former-commit-id: 28865c79032ed7ebb01c1a66521db58b0c4b078b

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/service/LoanService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/service/LoanService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1416a068e5ce2e6a5d815a9a008e1368e092b10e91eb7dc332874273af1e20eb
-size 2783
+oid sha256:06d63b86f19a6d7d63459c91a607b5fd55c48e87508099377a13de0c8dab65f5
+size 2980

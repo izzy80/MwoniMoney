466380bbf53d402e58d77861915d2b0834b19a55
Jaei <dobbykim0320@gmail.com>
2023-10-03 19:44:06 +0900

    [S09P22B310-268] Feat : myPage axios 연결 완료
    Former-commit-id: f7ed77a1a24e0ee1839aba0135c2c878ede79df1

--- frontend-web/src/modal/Challenge/ModalChildrenChallenge.tsx
+++ frontend-web/src/modal/Challenge/ModalChildrenChallenge.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2168d33c4d41c077a04cbce7476dc438c4248b12083b68cbe58f5781fa6c5b7
-size 5874
+oid sha256:8fc463e873064c6f8825c6c8c4b040b6dc8441a21d6417b4b15e5a0c3843351b
+size 5877

d74cd21928a4d216b8ce9546d33f9f70c7548172
박기택 <taekputer@gmail.com>
2023-10-03 16:29:54 +0900

    [S09P22B310-000] feat : 메인 페이지 default
    Former-commit-id: 079f365dfcbd58dc270dcd7b301261d34d3f8aa3

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/api/request/EditRewardRemainRequest.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/api/request/EditRewardRemainRequest.java
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2f80578bc9966d796595f25309ffed1e05ce75345b331acc85a8de2ccbd2bcc0
-size 209
+package com.ntt.mwonimoney.domain.member.api.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EditRewardRemainRequest {
+	private String childUUID;
+	private int rewardRemain;
+}
--- frontend-web/src/pages/ParentsPage.tsx
+++ frontend-web/src/pages/ParentsPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:742e7d083ea04b9b7aa788fc64f7555aa94d704242e4f1967c602d3490863b82
-size 3695
+oid sha256:c3fd4d2b0009ba8550f6db5ba3121b11c2bb7478e67eb8942537f5ef121b1afc
+size 3867

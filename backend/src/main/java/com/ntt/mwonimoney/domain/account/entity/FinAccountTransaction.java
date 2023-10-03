13ebfe6c5a204aadbc70e46f9662425e47c51d38
박기택 <taekputer@gmail.com>
2023-10-04 02:07:08 +0900

    [S09P22B310-000] feat : 백엔드 및 프론트 수정
    Former-commit-id: d8935a5bf5f76403988cfaf2d4ad403da8c39afd

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/FinAccountTransaction.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/FinAccountTransaction.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f264891cd3713dab95bea7c6f344e7446de3f940cc291d50502653068a4e690d
-size 1358
+oid sha256:6fcbc2c338e6b76223730676b8f6f2fbc21f18ed4ca06b0f0d55d12e0d2c3d62
+size 1359
--- frontend-web/src/components/Common/MoneyPage/MoneyTable.tsx
+++ frontend-web/src/components/Common/MoneyPage/MoneyTable.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f78cc32eb871d3ae9cad3e148318286e359df1b82e736de95f2b89ac3114cb3
-size 1609
+oid sha256:d3835265c3d25a76c16a9f0ff82ebaee0acefe2b5d263bd175d5a42d3a4933db
+size 1610

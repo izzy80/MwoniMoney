eefc4392518d87f0269e2913c7cbc2d47f7f5aca
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-03 18:05:16 +0900

    [S09P22B310-000]feat : 부모 은행 post axios
    Former-commit-id: a72c2bd9fcb5142047fda4534ee4c07db73892a2

--- frontend-web/src/modal/Loan/ModalLoan.tsx
+++ frontend-web/src/modal/Loan/ModalLoan.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21461aaa41b78aa40c953bb3ff5b4fdfba42ab1e38b474b9c5e06141b40e8904
-size 6094
+oid sha256:ee76f49a85d95f177c33c1e85271cb1aa853449a289731358c39f4458eccd6d1
+size 6486

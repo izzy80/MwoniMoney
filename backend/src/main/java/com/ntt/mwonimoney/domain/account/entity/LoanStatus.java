79fd3c6448bcce0746b0ff941e57f002928dcfb0
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-04 02:13:13 +0900

    [S09P22B310-000] feat : 가족은행 repay patch
    Former-commit-id: 131f09d7c558c27a705c6474a6a0c064b148403c

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/LoanStatus.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/entity/LoanStatus.java
@@ -1,5 +1,3 @@
-package com.ntt.mwonimoney.domain.account.entity;
-
-public enum LoanStatus {
-    APPROVAL, REJECTION, WATING, PAID;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb9ea0230537455dc5f75cc20be0ebc81cdb5cd395ad955b704f9625d27d3533
+size 117
--- backend/src/main/java/com/ntt/mwonimoney/domain/account/model/dto/LoanListRequestDto.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/model/dto/LoanListRequestDto.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c7b5922292b20dcaca2db7cda357eaa7400489a272c8a1757e9bd34a2199c0ab
-size 343
+package com.ntt.mwonimoney.domain.account.model.dto;
+
+import com.ntt.mwonimoney.domain.account.entity.LoanStatus;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+public class LoanListRequestDto extends PageToScroll{
+    private LoanListRequestStatus loanListRequestStatus;
+    private LoanMemberType loanMemberType;
+    private String childUUID;
+}

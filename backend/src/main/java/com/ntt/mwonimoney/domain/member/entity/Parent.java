7fcee5e2f88b8542ad940a000bb4dea12f6db3f4
tjdfkr011 <tjdfkr011@naver.com>
2023-10-03 19:00:35 +0900

    [S09P22B310-000] fix : 역할변경, 이메일 관련 버그 수정
    Former-commit-id: 01173be50a594df62468dfbbb450971666fb03e3

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Parent.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Parent.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73f4d373034f4d02161faec2e2dec1ae0123e0ff511d0fd9a5b40401faaf5304
-size 1329
+oid sha256:864b0c9621d9d2707940876a40a14c5bd21b1de687b4a4f10c341e683264d900
+size 1356

a810f420dd4f407f1c8214954474cb0449a2afb9
tjdfkr011 <tjdfkr011@naver.com>
2023-10-04 13:38:20 +0900

    [S09P22B310-000] feat : Bank API V2 생성
    Former-commit-id: 543776318d0779263969f742c1eb5fbb1d34dc1b

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/api/v1/FinAccountApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/api/v1/FinAccountApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf39b85dd6616ca3a14845505e4268604f1a1b9be5cdbb5248f768e3e1a6d7d4
-size 17641
+oid sha256:2998e3e1e686edbabaaac5eab823dc7cbbf8c25d1a213ae1c1f50f9b0570ddea
+size 17637

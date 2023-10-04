7d473dbb3288ecd69ccf0efd18a8875cbc91efa4
Jaei <dobbykim0320@gmail.com>
2023-10-05 02:48:04 +0900

    [S09P22B310-280] Design : 짜금통 사진삽입 시 이미지 변경 및 해지Axios 연결
    Former-commit-id: 79b054913085d6b82881e26efdafb608a2cd9860

--- frontend-web/src/components/Children/GoalCreate.tsx
+++ frontend-web/src/components/Children/GoalCreate.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32a5b7fba1b5cc6118555180ff8f59124883f8c7e36d413e7f309b450ee8bf9c
-size 5999
+oid sha256:070863792942dfa64d2edc77d2f0bbba9178d2b975a8343cecd9834582785b30
+size 6192

0011116add5480fbd2159e22a5f2629f17d3a12f
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-05 00:55:53 +0900

    [S09P22B310-295] design : 가족은행 css
    Former-commit-id: 2b2641cb706606a3900853ccfa27c932e3d44595

--- frontend-web/src/components/Common/Bank/Card.tsx
+++ frontend-web/src/components/Common/Bank/Card.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b74cf956f4cec47d795ba94a07d492bbae96810d598ccef043e63277990d9d22
+oid sha256:280dd399f1e7fe53e89b6d5972141ce996b60dba71b73ca158115b132e50a891
 size 950
--- frontend-web/src/components/Common/Bank/LoanList.tsx
+++ frontend-web/src/components/Common/Bank/LoanList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a82e777690eeff8bf660702c4a54b3c6bf720e045b9b8ec76e2ea3e6defdd619
-size 6198
+oid sha256:29ed34840a1448ab8c7944ec11aeb8ff734b42373d7f467e3db6ed8837c07cab
+size 6135

5b4041b51906353655100f61af671d72e608a63e
Jaei <dobbykim0320@gmail.com>
2023-10-04 19:25:14 +0900

    [S09P22B310-280] Design : 채팅 디자인 모바일 확인용
    Former-commit-id: a00dc8ab571c2dbbe05a623271d835bb97ff1e41

--- frontend-web/src/components/Common/MoneyPage/MoneyParentsPage.tsx
+++ frontend-web/src/components/Common/MoneyPage/MoneyParentsPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a75689abdcc15acf82022656a9533a01e925f423a2413c25778711a93dd46dd
-size 3935
+oid sha256:44794113509f599ffd5eb23f5dd42b032a4ca1f7386834ed5ab4cf4fa2fa5b6a
+size 4002

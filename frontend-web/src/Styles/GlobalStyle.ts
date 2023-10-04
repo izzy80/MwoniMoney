583c7efaf958b7b2da285fe874e9d716f8626b80
aldms8960 <45choi22@gmail.com>
2023-10-04 22:37:20 +0900

    FiX: font 삭제
    Former-commit-id: 66bf569bbaa806bc9316a6c33cef55daa1505735

--- frontend-web/src/Styles/GlobalStyle.ts
+++ frontend-web/src/Styles/GlobalStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94ffe2a9a2c73d89ea974d69141517d9c6c336d1be29d0cef41f194a4a0c54e5
-size 608
+oid sha256:ec1e6f44b9cf58859c910d9b16fe2d1c85aa74f3a0800b6050349d019b063f4f
+size 614

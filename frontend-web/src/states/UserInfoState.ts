2ed2a51e25fc58154f0fd4390d4c58ee149ae1dc
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-04 09:53:50 +0900

    [S09P22B310-288]feat : 가족은행 total axios
    Former-commit-id: efc3588e43f967e523bb650c1e0f3549b524b81d

--- frontend-web/src/states/UserInfoState.ts
+++ frontend-web/src/states/UserInfoState.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c33c648ecacd8a0f214cf930f3c37e6d613cfbd38d113dcabe7cffe39937c61
-size 1217
+oid sha256:c898cfe11ccce746cab7f641938e981aa36bdf8a4e3460494ca5e76cb9f0f8f6
+size 1264

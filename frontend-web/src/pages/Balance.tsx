ebef2db45593f32d3539783d5e41a8c7e876f696
tjdfkr011 <tjdfkr011@naver.com>
2023-10-05 00:29:04 +0900

    [S09P22B310-000] feat : 밸런스게임 수정
    Former-commit-id: 4418bb6889c5e10325cd584db2c797557184146e

--- frontend-web/src/pages/Balance.tsx
+++ frontend-web/src/pages/Balance.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fe45f93ceb3c5d31f1778fdc1ac463ceecbb8ffea365678b93a933cd9e25d7a
-size 4786
+oid sha256:dc2d9430b8614c6ce9d67d40146a9e9cd444a1310ac109a5e2acf68ad9f6e298
+size 4796

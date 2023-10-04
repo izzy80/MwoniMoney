0d2d6a7b034a086b82a993aedf0479bdda7965b9
tjdfkr011 <tjdfkr011@naver.com>
2023-10-04 19:51:56 +0900

    [S09P22B310-000] feat : 짜금통 자동 저장 추가
    Former-commit-id: f2cc46b1280c6a76ce0e4319e98753ddbfd6be22

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/service/v2/FinAccountTransactionServiceImplV2.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/service/v2/FinAccountTransactionServiceImplV2.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bc613cbc3a3236f085267e9d9d44fef839c5a0c1c5e4c8910ecdc24c1cfc4f6
-size 4376
+oid sha256:c6cf25ac2e559881dfba1afaa49f7439c4c0724e0faca5db560a764dda2b08ae
+size 4910

8985585ffb506f2ba8fd5bc61924d9200f5a6353
tjdfkr011 <tjdfkr011@naver.com>
2023-10-04 14:20:50 +0900

    [S09P22B310-000] fix : Account Get 오류 수정
    Former-commit-id: f2d036cfd610c115c654684b30f86777312989db

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/FinAccountTransactionRepositoryCustomImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/FinAccountTransactionRepositoryCustomImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0820bcebee79203c4e99f1cc32f64ffba59c98d170b18b09c38b261b5f2a1945
-size 1040
+oid sha256:94a53cd416dd93b1c372140f4fdf567467fa8e58453fb1de0edee73f567b34eb
+size 1028

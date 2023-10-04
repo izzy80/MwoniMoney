39ba86380a0a82184d4abdb748c6ac4b8a52263e
윤민재 <w124564@naver.com>
2023-10-05 01:12:04 +0900

    [S09P22B310-000] feat : 정기용돈 기능 추가
    Former-commit-id: 8ad411fbdd246dce85870d1c5e45b816304ec71e

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/repository/MemberRepository.java
@@ -1,9 +1,3 @@
-package com.ntt.mwonimoney.domain.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ntt.mwonimoney.domain.member.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:18e215114b159d90b0a48cfa5722f47170a433a8da6e5da364c871234b96bddf
+size 414

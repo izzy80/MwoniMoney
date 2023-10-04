9ae6385a380098728668fc243e24a1f0ddd63692
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-04 09:53:50 +0900

    [S09P22B310-288]feat : 가족은행 total axios
    Former-commit-id: 686383165a6095ab7aeba8e6bf626c91fa2d7c0c

--- frontend-web/src/components/Common/Bank/BankGraph.tsx
+++ frontend-web/src/components/Common/Bank/BankGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea7a07853cd3257c110a166e98e4e09464d40e5a267939bd936a121e2497da42
-size 1726
+oid sha256:58428165a4e1cb3f3693c6e5d32bf2fdbde01fe917b09e1f21e0de141e37580f
+size 1788

7ebe6645941f8cafe230ecc8090ea2d405db502b
박기택 <taekputer@gmail.com>
2023-10-05 00:22:58 +0900

    [S09P22B310-000] feat : Plus 버튼 추가
    Former-commit-id: 725f55aa6cd614ec8df64a6af5ecf62111a0522a

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameService.java
@@ -1,15 +1,3 @@
-package com.ntt.mwonimoney.domain.game.service;
-
-import org.springframework.data.domain.Slice;
-
-import com.ntt.mwonimoney.domain.game.api.request.BalanceGameListRequest;
-import com.ntt.mwonimoney.domain.game.model.dto.BalanceGameDto;
-
-public interface BalanceGameService {
-
-	Slice getEndBalanceGames(BalanceGameListRequest request);
-
-	BalanceGameDto getTodayBalanceGame();
-
-	BalanceGameDto getBalanceGameInfo(Long balanceGameIdx);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ae920f52998c6ef252c22121051b4bb8823e2b660af60123bede4964e1b5f51b
+size 433

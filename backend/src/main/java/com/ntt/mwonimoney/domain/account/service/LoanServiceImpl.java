9cace45c502fedb03ae68e1381160fedceee507b
aldms8960 <45choi22@gmail.com>
2023-10-04 23:55:26 +0900

    Fix: total 소수점 자리 수정
    Former-commit-id: 27e9e80f2da890ddccfcdeefcd88069ec0b88d69

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/service/LoanServiceImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/service/LoanServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53b1c4f287b92cfca8e5adc4985e7f85c239ca5830e4e42e3fc745955f866906
-size 3575
+oid sha256:ea57cf35fcf6eed25792a03faf44e8b3f3e38f5132505cc14f15824eac0fd1b1
+size 3632

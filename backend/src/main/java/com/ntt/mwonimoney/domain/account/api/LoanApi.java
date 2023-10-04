fcf2fcf4e1f18e7df5c8565946196f5f2f37caf1
윤민재 <w124564@naver.com>
2023-10-04 09:43:08 +0900

    [S09P22B310-000] fix : totalBalance 조회
    Former-commit-id: 8cf30f6ed4c0f82b96aa1bbb05ac27339a1c0c76

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/api/LoanApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/api/LoanApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ef97214caea2f6f237cd94da698ceca7c8dce1a76a3ca527abf4818771a536c
-size 6321
+oid sha256:b678fd30ab681933e1f3b758cde9355936380748202517279570f301683f2b13
+size 6370

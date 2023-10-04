1523d3c9eedabe4eae9f38cfd14484ade1d739cc
aldms8960 <45choi22@gmail.com>
2023-10-04 22:44:19 +0900

    Fix: 글씨체 url 수정
    Former-commit-id: 4496c0731f95da8ae95ea1f87a69f43119ab45cc

--- frontend-web/src/Styles/GlobalStyle.ts
+++ frontend-web/src/Styles/GlobalStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94ffe2a9a2c73d89ea974d69141517d9c6c336d1be29d0cef41f194a4a0c54e5
-size 608
+oid sha256:ff639b86321e5544e7d44d01300b5680c543a68cbf620235b77837a11b881279
+size 589

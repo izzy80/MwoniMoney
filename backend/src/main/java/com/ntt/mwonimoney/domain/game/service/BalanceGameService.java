3afffd71ddf2b2fc855cebfd968ce3ac198ba7ef
tjdfkr011 <tjdfkr011@naver.com>
2023-10-05 00:20:15 +0900

    [S09P22B310-000] feat : 밸런스게임 수정
    Former-commit-id: 2ba43989e4ee8d26cb4c1a9950cd1c33741b80e2

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameService.java
@@ -1,15 +1,3 @@
-package com.ntt.mwonimoney.domain.game.service;
-
-import org.springframework.data.domain.Slice;
-
-import com.ntt.mwonimoney.domain.game.api.request.BalanceGameListRequest;
-import com.ntt.mwonimoney.domain.game.model.dto.BalanceGameDto;
-
-public interface BalanceGameService {
-
-	Slice getEndBalanceGames(BalanceGameListRequest request);
-
-	BalanceGameDto getTodayBalanceGame();
-
-	BalanceGameDto getBalanceGameInfo(Long balanceGameIdx);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:747b3b838aa6e15ba5d2c0379938fe2b9c21adaa81e928d8f62899dd4cbc5a6d
+size 589

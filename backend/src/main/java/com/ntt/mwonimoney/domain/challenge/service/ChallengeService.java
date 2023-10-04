c8adb1b9e504d55e61ca74577905ab54c1c03308
윤민재 <w124564@naver.com>
2023-10-04 13:13:25 +0900

    [S09P22B310-000] feat : service interface 생성
    Former-commit-id: 820bb70968cb5b50cee5ffac05157d29b6ffccb8

--- backend/src/main/java/com/ntt/mwonimoney/domain/challenge/service/ChallengeService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/challenge/service/ChallengeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a96070bbd2b352bda79c93be9a0a5d5e79efc3b9f46db89424b33319ab9fb6d
-size 6260
+oid sha256:da356602a40a3545cd7749e704b7baf34c4df1aba48e894ce510cd63d300de03
+size 826

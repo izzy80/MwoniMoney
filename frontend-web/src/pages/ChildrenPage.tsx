b7a2f46d272cbc27507f655192b5c16f76a40c30
tjdfkr011 <tjdfkr011@naver.com>
2023-10-05 00:05:17 +0900

    [S09P22B310-000] feat : 밸런스게임 수정
    Former-commit-id: d07f633dac23f5a9baad9edc4c03c0490323fe01

--- frontend-web/src/pages/ChildrenPage.tsx
+++ frontend-web/src/pages/ChildrenPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eed92cc5602046cca78a050d6f4a32386a21c16b2a0bd9fcb382c1a5cede7a9b
-size 5816
+oid sha256:d22a3612a62a0422b71010fd0787df106c31eb3da8d98413ec04706059fd5d66
+size 5225

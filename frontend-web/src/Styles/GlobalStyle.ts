052c1b94408907a6a647b6afd28cc1088bf552d4
aldms8960 <45choi22@gmail.com>
2023-10-04 22:28:56 +0900

    Fix: 글꼴 설정
    Former-commit-id: 46f2410a6464b3d16691ed4182cdea10907b6f53

--- frontend-web/src/Styles/GlobalStyle.ts
+++ frontend-web/src/Styles/GlobalStyle.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df12e13b76d100e81dacd85ded7170a93eabc3c13f38805600414f137aa3efae
-size 614
+oid sha256:94ffe2a9a2c73d89ea974d69141517d9c6c336d1be29d0cef41f194a4a0c54e5
+size 608

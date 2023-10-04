90bd95c59d07b638338ba7712a5f0db2e4da2c77
윤민재 <w124564@naver.com>
2023-10-04 15:35:04 +0900

    [S09P22B310-000] fix : 챌린지 버그 수정
    Former-commit-id: e98df235fc692af47fefc12b79594761217cafac

--- backend/src/main/java/com/ntt/mwonimoney/domain/challenge/api/ChallengeApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/challenge/api/ChallengeApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be5437ff6ba3cc1a6b746322d9ab12aa5a918475a8b4ddb5ff26585237ebea13
-size 7559
+oid sha256:6f17e49856fa84d8d74ecc0112dd1fc2ee2a0fb73d296ee306e26d7a9b997994
+size 6792

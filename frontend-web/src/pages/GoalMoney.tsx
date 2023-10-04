50b70a8f3b75769499bec89cf092c9a0d073af32
Jaei <dobbykim0320@gmail.com>
2023-10-04 13:13:19 +0900

    [S09P22B310-268] Feat : 짜금통 끝
    Former-commit-id: fd916e088cb97efe6dc136e92b9c7992a9fb7d86

--- frontend-web/src/pages/GoalMoney.tsx
+++ frontend-web/src/pages/GoalMoney.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b749bb8b05a03110126322462d429ae40713186fd67962f8e12e7b14abd78475
-size 8481
+oid sha256:bccf7b71ad7f1d24e02cf194f3c727bb131d917c7e2d24b9e96879127d862ef8
+size 8628

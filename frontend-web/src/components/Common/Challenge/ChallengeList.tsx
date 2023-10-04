242970c628ddf89a441818ed4150d94416aaaabb
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-04 15:20:14 +0900

    [S09P22B310-292] fix : 챌린지 axios requestparam 추가
    Former-commit-id: 376d134ed565261d96ed84f84cb811d54e65b36d

--- frontend-web/src/components/Common/Challenge/ChallengeList.tsx
+++ frontend-web/src/components/Common/Challenge/ChallengeList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef4ddeba84660e4addd26d2d371c73a67b2a606037cea14684082aed12c7fa8c
-size 8801
+oid sha256:4679634b156de91d4dd938684e57f3eda72be57faa5f364aa44005980cb9efdb
+size 9397

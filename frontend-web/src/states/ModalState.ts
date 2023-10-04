ed6c1152396581c9181c53584f38d521c9c2699a
Jaei <dobbykim0320@gmail.com>
2023-10-04 20:26:12 +0900

    [S09P22B310-280] Design : chatting 디자인 60%
    Former-commit-id: 904c89b93d5c8d5f15842ce25e468ec006cff882

--- frontend-web/src/states/ModalState.ts
+++ frontend-web/src/states/ModalState.ts
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1027d844d9749981ba986983a27eb7ecd514eccc6cf2244f34bc82b99c2aeade
-size 117
+import { atom } from "recoil";
+
+export const ModalState = atom<boolean>({
+  key: "ModalState",
+  default: false,
+});

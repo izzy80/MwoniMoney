2064647acbecfebb14d3c57d525639ff1bf99450
박기택 <taekputer@gmail.com>
2023-10-05 14:16:44 +0900

    [S09P22B310-000] feat : 글씨 색 바꾸기
    Former-commit-id: a6e5bedde3bac33b851d24ce39367cf7f2b32277

--- frontend-web/src/components/Common/MoneyPage/MoneyChildPage.tsx
+++ frontend-web/src/components/Common/MoneyPage/MoneyChildPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:008aa4eeadc770f54471a09a7433501bf1f32bd9f6ca0974bdea438dd83cd166
-size 6446
+oid sha256:f55c0d7a3502a94f94390b32a45aaa61fa850def5c2db12105b5efa728a1b06a
+size 6490

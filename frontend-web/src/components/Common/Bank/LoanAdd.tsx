0c09b15cfa85b86253e4ec9e2a2f434f7f707a85
Jaei <dobbykim0320@gmail.com>
2023-10-05 14:35:40 +0900

    [S09P22B310-276] Desigan : 추가버튼 디자인 수정
    Former-commit-id: 014cb1353fd0bc2b0efe6c7f5666c7b4be3ebf38

--- frontend-web/src/components/Common/Bank/LoanAdd.tsx
+++ frontend-web/src/components/Common/Bank/LoanAdd.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87ddf5bd99856e2b5d6719a8c7abdcca3059fef5aa2d07874dc7268444b341ad
-size 992
+oid sha256:221b823b9a2b40cffbc451005dcef001e7035df701c6f3212248852b9cff5990
+size 1073

20c1c489f49da1fc6e6ef61a9c20b4a4c3b84fe7
박기택 <taekputer@gmail.com>
2023-10-06 01:03:14 +0900

    [S09P22B310-000] feat : Quiz 연결
    Former-commit-id: 8152001bd54b3d7b46baa341fa24cc7c13a971c0

--- frontend-web/src/components/Common/Quiz/Quiz.tsx
+++ frontend-web/src/components/Common/Quiz/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14cf3cf584de5bb73a16615453a1ccdfbf9f8d6348c092884a734693fbd4433a
-size 6628
+oid sha256:2892979ad69a1a292944c9ae4a49b4e4e2ed8597d56b934bb0f096d51e3ec0d5
+size 7078

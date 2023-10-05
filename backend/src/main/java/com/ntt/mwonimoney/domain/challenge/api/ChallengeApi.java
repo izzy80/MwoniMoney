991348fbf86528c66f899c2c863f1d4fd52aa7b4
박기택 <taekputer@gmail.com>
2023-10-05 11:11:00 +0900

    [S09P22B310-000] feat : 트랜잭션 수정
    Former-commit-id: 110ea931f107bee1870edc72c5141acd0c537bcf

--- backend/src/main/java/com/ntt/mwonimoney/domain/challenge/api/ChallengeApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/challenge/api/ChallengeApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb12202eb2930cbb85dbb59c8222fbc6f0e3f3be6b3700265a23f04442d84e53
-size 7452
+oid sha256:e7a472c44241557cdfcbc73c9800f00f731d109991c81a246d2f7e6d4d4c0f50
+size 7433

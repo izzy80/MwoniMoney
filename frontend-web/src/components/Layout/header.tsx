c01fc94047ac271c8ff8df434d31a6d2532d72d8
Jaei <dobbykim0320@gmail.com>
2023-10-05 14:50:40 +0900

    [S09P22B310-276] Page Develop
    Former-commit-id: f185656156d0fe188603c393d9d75f4595f3caeb

--- frontend-web/src/components/Layout/header.tsx
+++ frontend-web/src/components/Layout/header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f653d3d2c2403dafec6f244ed1733832d2bd97250e467932c10b9fe4840c4db
-size 1833
+oid sha256:b0e2d652f2ab7549c0bb2a29223353c884349a9690f43d1e7d05b99bce8b8d63
+size 1835

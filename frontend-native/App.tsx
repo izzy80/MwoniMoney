4a7ee3615fa2796e6db2b88ace64769ca95df30d
윤민재 <w124564@naver.com>
2023-10-06 02:13:24 +0900

    [S09P22B310-000] feat : fcm 개발
    Former-commit-id: 1b6d65ae0f50e8a0fa286af68fd4d801ffca0123

---
+++ frontend-native/App.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ad1929e7c607a07ce8524fc4de47d6496b932914c8c101ceb1d2bacd4a60600
+size 3581
--- frontend-native/index.js
+++ frontend-native/index.js
@@ -1,9 +1,3 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+version https://git-lfs.github.com/spec/v1
+oid sha256:d57cc391f71de4827673c5c9d82098355b1091376d89b41ec893217dc65042a2
+size 263

39ee2873db8e15838881bdc82e6790b511a0ac79
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-05 09:32:32 +0900

    [S09P22B310-154]feat : 부모 챌린지 완료 api에 송금로직 추가
    Former-commit-id: f816737e363737f963b0fdc4165c394ebb3020f7

--- backend/src/main/java/com/ntt/mwonimoney/domain/challenge/api/ChallengeApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/challenge/api/ChallengeApi.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcf2f82b544f4521e47a5b1072e049355f95dd42feb867dbb3d977ffa817f5c2
-size 6447
+oid sha256:fb12202eb2930cbb85dbb59c8222fbc6f0e3f3be6b3700265a23f04442d84e53
+size 7452

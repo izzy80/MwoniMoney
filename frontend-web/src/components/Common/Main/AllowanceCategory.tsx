0215d8bb60459c00953980f41b4356b16f0cd2ce
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-05 15:52:51 +0900

    [S09P22B310-293] design : 버튼 css
    Former-commit-id: 0153a12818e774142d5b4d83b6c7f858a83c0235

--- frontend-web/src/components/Common/Main/AllowanceCategory.tsx
+++ frontend-web/src/components/Common/Main/AllowanceCategory.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6d2e02cdc744c083cba5f0549e4399a2ea8d3979c49ecfe167df7f01a36f175
-size 814
+oid sha256:20e35fa5d9ad7dfb96eecf8171a22ed9b12a24a4a30b39376c6823e23c1b2b1d
+size 1016

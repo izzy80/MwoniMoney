a9f77a478c845bcc7a91b88b34f225a0219e6d8a
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-06 03:00:41 +0900

    [S09P22B310-293] design : 챌린지 리스트 css
    Former-commit-id: 30cfc58151bb55426f2c505b82cc0780bca0326c

--- frontend-web/src/components/Common/Challenge/ChallengeList.tsx
+++ frontend-web/src/components/Common/Challenge/ChallengeList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8261783b2c71eb182dc6db1eb34c53ba6c9cb5b693c0b391957d8e197280777c
-size 10823
+oid sha256:4c977655a6aafafd675deee8e2d02723a534db1e76e3041ec7d9ca917aeefbc9
+size 10997
--- frontend-web/src/pages/ChildrenPage.tsx
+++ frontend-web/src/pages/ChildrenPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6d7639e914e4ac8174b2ee7e422f463843b5e0ebc14d6e0bf873ce398b7bbe5
+oid sha256:b8bd3d1e4605fdf2f8d2fe9005c23dff07d84173655c77acef5268be7d8d7cea
 size 5343

7a5641677d8dc1f785e5d0e50beecd6a1a4dbc51
Jaei <dobbykim0320@gmail.com>
2023-11-04 15:53:16 +0900

    Refactor: BankComponent 정리
    Former-commit-id: 2dcb76f9e8d55a0f8d77d1a45dbe1012dbf47b14

--- frontend-web/src/components/Common/Bank/GoGoalMoney.tsx
+++ frontend-web/src/components/Common/Bank/GoGoalMoney.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27a8c037189994f7f78cb4d33e6262399ba6a25fd8b02913fb89deff43ec46d5
-size 3005
+oid sha256:0b79ffc59d10d309abf91012dc5406db52a510d0ae748e1adc8c266d0619104e
+size 2917
--- frontend-web/src/components/Common/Bank/LoanRepay.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f80d074b96eab2caeda44b9fd917bf964a27986fd87b91eccfad0087dc4bbda1
-size 1585

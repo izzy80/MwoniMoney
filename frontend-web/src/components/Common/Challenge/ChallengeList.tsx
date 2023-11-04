12cf5e19fd30380d0fef381594d090b1fb49d641
Jaei <dobbykim0320@gmail.com>
2023-11-04 15:57:59 +0900

    Refactor: ChallengeComponent 정리
    Former-commit-id: 97f8e903ff96d2834cd37e9dd7ba50022db7aa34

--- frontend-web/src/components/Common/Challenge/ChallengeList.tsx
+++ frontend-web/src/components/Common/Challenge/ChallengeList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c977655a6aafafd675deee8e2d02723a534db1e76e3041ec7d9ca917aeefbc9
-size 10997
+oid sha256:e06ff22b6a2e209dc5265768baa59fa7c5494301a7abd2183521ee6f02740cbc
+size 10598

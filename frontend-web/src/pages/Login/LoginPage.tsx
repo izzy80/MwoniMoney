372592aabcb847286d89b9530d6849331b8a0428
Jaei <dobbykim0320@gmail.com>
2023-11-04 17:09:51 +0900

    Refactor: Login 정리
    Former-commit-id: 2d4fa2f406e715d8f7632522808c8fbf9a7cced8

--- frontend-web/src/pages/Login/LoginPage.tsx
+++ frontend-web/src/pages/Login/LoginPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcd44565c0d3910943f8ea16cac41abe85e32b7498c09720b06c7267f40810e1
-size 2348
+oid sha256:1683380d13acdefba1d8d80eda70a75d76d5464847c771e497f56525bbe0b68f
+size 1965

d19128f39eb8801149a0b0dd153f3d517e4df75c
Jaei <dobbykim0320@gmail.com>
2023-11-04 16:37:16 +0900

    Refactor: LayoutComponent 정리
    Former-commit-id: 926d8178af7cd05e01a0b56574cbe4aa021a6dd3

--- frontend-web/src/components/Layout/footer.tsx
+++ frontend-web/src/components/Layout/footer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f71ab14ab854a6748c3fff5fe2fd7cf53683a839e186edacf99aaddab48d0a6
-size 1050
+oid sha256:9840bc448d58a3a5d4ef408b6c5f5e09dab6fcdc78d2fb0c59c009e39fc0e483
+size 888
